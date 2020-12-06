8c62d6edff8815af8019a22c052f8cfb377f72a6
turquoise-hexagon <camille@lzr.pw>
2020-12-06 14:18:46 +0000

    update day_6.scm

--- 2020/day_6/day_6.scm
+++ 2020/day_6/day_6.scm
@@ -1,39 +1,26 @@
 (import (chicken io)
         (chicken process-context)
         (chicken irregex)
-        (chicken string)
-        (srfi 69)
-        (srfi 1))
+        (srfi 113)
+        (srfi 128))
+
+(define comparator
+  (make-comparator char? char=? char<? char->integer))
 
 (define (import-input path)
   (map
     (lambda (str)
-      (let ((hash (make-hash-table))
-            (lst  (string-split str)))
-        (for-each
-          (lambda (char)
-            (hash-table-set! hash char (add1 (hash-table-ref/default hash char 0))))
-          (string->list (apply string-append lst)))
-        (list hash (length lst))))
+      (map
+        (lambda (lst)
+          (list->set comparator lst))
+        (map string->list (irregex-split "\n" str))))
     (irregex-split "\n\n" (read-string #f (open-input-file path)))))
 
-(define (solve/1 input)
-  (display (apply + (map length (map hash-table-keys (map car input)))))
-  (newline))
-
-(define (solve/2 input)
-  (display (apply + (map
-                      (lambda (lst)
-                        (let ((hash (car  lst))
-                              (numb (cadr lst)))
-                          (length (filter
-                                    (lambda (key)
-                                      (= (hash-table-ref hash key) numb))
-                                    (hash-table-keys hash)))))
-                      input)))
+(define (solve proc input)
+  (display (apply + (map set-size (map (lambda (sets) (apply proc sets)) input))))
   (newline))
 
 (let ((path (car (command-line-arguments))))
   (let ((input (import-input path)))
-    (solve/1 input)
-    (solve/2 input)))
+    (solve set-union        input)
+    (solve set-intersection input)))
