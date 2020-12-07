921270a7c30a2ef166c5774ceb38eb52e323ad72
turquoise-hexagon <camille@lzr.pw>
2020-12-07 21:28:04 +0000

    add day_7

---
+++ 2020/day_1/test.scm
@@ -0,0 +1,36 @@
+(import (chicken io)
+        (chicken process-context))
+
+(define (import-input path)
+  (map string->number (read-lines (open-input-file path))))
+
+(define (solve/1 input)
+  (display
+    (call/cc
+      (lambda (return)
+        (do ((i input (cdr i))) ((null? i))
+          (do ((j (cdr i) (cdr j))) ((null? j))
+            (let ((a (car i))
+                  (b (car j)))
+              (when (= 2020 (+ a b))
+                (return (* a b)))))))))
+  (newline))
+
+(define (solve/2 input)
+  (display
+    (call/cc
+      (lambda (return)
+        (do ((i input (cdr i))) ((null? i))
+          (do ((j (cdr i) (cdr j))) ((null? j))
+            (do ((k (cdr j) (cdr k))) ((null? k))
+              (let ((a (car i))
+                    (b (car j))
+                    (c (car k)))
+                (when (= 2020 (+ a b c))
+                  (return (* a b c))))))))))
+  (newline))
+
+(let ((path (car (command-line-arguments))))
+  (let ((input (import-input path)))
+    (solve/1 input)
+    (solve/2 input)))
