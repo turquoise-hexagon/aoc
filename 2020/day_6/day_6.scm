a532a56495e732fb4d63673dd8278aa3c3f5a2e3
turquoise-hexagon <camille@lzr.pw>
2020-12-08 14:03:22 +0000

    update day_6.scm

--- 2020/day_6/day_6.scm
+++ 2020/day_6/day_6.scm
@@ -10,14 +10,12 @@
 (define (import-input path)
   (map
     (lambda (str)
-      (map
-        (lambda (lst)
-          (list->set comparator lst))
-        (map string->list (irregex-split "\n" str))))
+      (map (cut list->set comparator <>)
+           (map string->list (irregex-split "\n" str))))
     (irregex-split "\n\n" (read-string #f (open-input-file path)))))
 
 (define (solve proc input)
-  (display (apply + (map set-size (map (lambda (sets) (apply proc sets)) input))))
+  (display (apply + (map set-size (map (cut apply proc <>) input))))
   (newline))
 
 (let ((path (car (command-line-arguments))))
