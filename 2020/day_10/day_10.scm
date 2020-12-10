a4904192ad17f402b059cad88894b8d1ca7560e5
turquoise-hexagon <camille@lzr.pw>
2020-12-10 15:41:43 +0000

    update day_10.scm

--- 2020/day_10/day_10.scm
+++ 2020/day_10/day_10.scm
@@ -3,9 +3,15 @@
         (chicken sort)
         (srfi 1))
 
+(define (diffs input)
+  (fold
+    (lambda (a b acc)
+      (cons (- b a) acc))
+    (list) input (cdr input)))
+
 (define (import-input path)
   (let ((lst (map string->number (read-lines (open-input-file path)))))
-    (cons 0 (foldr cons (list (+ (apply max lst) 3)) (sort lst <)))))
+    (diffs (cons 0 (foldr cons (list (+ (apply max lst) 3)) (sort lst <))))))
 
 (define (tribonacci n)
   (let tribonacci/h ((a 0) (b 0) (c 1) (n n))
@@ -13,16 +19,9 @@
         a
         (tribonacci/h b c (+ a b c) (- n 1)))))
 
-(define (diffs input)
-  (fold
-    (lambda (a b acc)
-      (cons (- b a) acc))
-    (list) input (cdr input)))
-
 (define (solve/1 input)
-  (let ((diffs (diffs input)))
-    (print (* (length (filter (cut = <> 1) diffs))
-              (length (filter (cut = <> 3) diffs))))))
+  (print (* (length (filter (cut = <> 1) input))
+            (length (filter (cut = <> 3) input)))))
 
 (define (solve/2 input)
   (print (apply * (map
@@ -33,7 +32,7 @@
                         (if (= a 1)
                             (cons (+ 1 (car acc)) (cdr acc))
                             (cons 0 acc)))
-                      (list 0) (diffs input))))))
+                      (list 0) input)))))
 
 (let ((path (car (command-line-arguments))))
   (let ((input (import-input path)))
