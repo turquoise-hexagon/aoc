40067a0a6ad86b728acd5c33c30974e55bb34a7b
turquoise-hexagon <camille@lzr.pw>
2020-12-15 18:21:47 +0000

    update day_05.scm

--- 2020/day_05/day_05.scm
+++ 2020/day_05/day_05.scm
@@ -1,5 +1,6 @@
 (import (chicken io)
         (chicken process-context)
+        (chicken irregex)
         (srfi 1))
 
 (define (import-input path)
@@ -7,11 +8,10 @@
 
 (define (get-seat-id boarding-pass)
   (define (get-seat/h lst)
-    (do ((lst (reverse lst) (cdr lst))
-         (cnt 0 (+ cnt 1))
-         (acc 0 (case (car lst)
-                  ((#\B #\R) (+ acc (expt 2 cnt))) (else acc))))
-      ((null? lst) acc)))
+    (let* ((str (list->string lst))
+           (str (irregex-replace/all "[BR]" str "1"))
+           (str (irregex-replace/all "[FL]" str "0")))
+      (string->number str 2)))
   (+ (* (get-seat/h (take boarding-pass 7)) 8)
      (get-seat/h (drop boarding-pass 7))))
 
@@ -19,13 +19,16 @@
   (print (apply max ids)))
 
 (define (solve/2 ids)
-  (print (call/cc
-           (lambda (return)
-             (for-each
-               (lambda (id)
-                 (cond ((not (member (+ id 1) ids)) (return (+ id 1)))
-                       ((not (member (- id 1) ids)) (return (- id 1)))))
-               ids)))))
+  (let ((alist (map list ids)))
+    (print (call/cc
+             (lambda (return)
+               (for-each
+                 (lambda (id)
+                   (when (and (not (assoc id alist))
+                              (assoc (+ id 1) alist)
+                              (assoc (- id 1) alist))
+                     (return id)))
+                 (iota (+ (* 127 8) 7))))))))
 
 (let ((path (car (command-line-arguments))))
   (let ((ids (map get-seat-id (import-input path))))
