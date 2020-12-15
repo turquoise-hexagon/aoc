88e74bee31fc97b130239ad4edfa5f5eacb31a3f
turquoise-hexagon <camille@lzr.pw>
2020-12-15 23:36:53 +0000

    update day_15.scm

--- 2020/day_15/day_15.scm
+++ 2020/day_15/day_15.scm
@@ -2,23 +2,23 @@
         (chicken process-context)
         (chicken string)
         (matchable)
-        (srfi 69)
         (srfi 1))
 
 (define (import-input path)
   (map string->number (string-split (read-string #f (open-input-file path)) ",\n")))
 
 (define (solve input n)
-  (let ((hash (alist->hash-table (fold
-                                   (lambda (a b acc)
-                                     (cons (cons a b) acc))
-                                   (list) input (iota (length input) 1 1)))))
+  (let ((memory (make-vector n #f)))
+    (for-each
+      (lambda (a b)
+        (vector-set! memory a b))
+      input (iota (length input) 1 1))
     (let solve/1/h ((turn (length input)) (last (car (reverse input))))
       (if (= turn n) (print last)
-          (let ((new (match (hash-table-ref/default hash last #f)
+          (let ((new (match (vector-ref memory last)
                             ((? number? a) (- turn a))
                             (_ 0))))
-            (hash-table-set! hash last turn)
+            (vector-set! memory last turn)
             (solve/1/h (+ turn 1) new))))))
 
 (let ((path (car (command-line-arguments))))
