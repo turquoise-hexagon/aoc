8b3a18de310a79a0f47786490148db9c2d1f49f7
turquoise-hexagon <camille@lzr.pw>
2020-12-15 13:47:26 +0000

    update day_05.scm

--- 2020/day_05/day_05.scm
+++ 2020/day_05/day_05.scm
@@ -8,7 +8,7 @@
 (define (get-seat-id boarding-pass)
   (define (get-seat/h lst)
     (do ((lst (reverse lst) (cdr lst))
-         (cnt 0 (add1 cnt))
+         (cnt 0 (+ cnt 1))
          (acc 0 (case (car lst)
                   ((#\B #\R) (+ acc (expt 2 cnt))) (else acc))))
       ((null? lst) acc)))
@@ -23,8 +23,8 @@
            (lambda (return)
              (for-each
                (lambda (id)
-                 (cond ((not (member (add1 id) ids)) (return (add1 id)))
-                       ((not (member (sub1 id) ids)) (return (sub1 id)))))
+                 (cond ((not (member (+ id 1) ids)) (return (+ id 1)))
+                       ((not (member (- id 1) ids)) (return (- id 1)))))
                ids)))))
 
 (let ((path (car (command-line-arguments))))
