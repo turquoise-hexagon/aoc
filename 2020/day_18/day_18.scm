a4cdf8f98143eb3038cd598ee708e0156bd2834b
turquoise-hexagon <camille@lzr.pw>
2020-12-18 20:41:08 +0000

    update day_18.scm

--- 2020/day_18/day_18.scm
+++ 2020/day_18/day_18.scm
@@ -14,22 +14,22 @@
 (define (evaluate/1 str)
   (let ((lst (string-split str " ")))
     (let evaluate/1/h ((lst (cdr lst)) (acc (string->number (car lst))))
-      (if (null? lst)
-          acc
-          (evaluate/1/h (cdr lst) (match lst
-                                    (("+" n . _) (+ acc (string->number n)))
-                                    (("*" n . _) (* acc (string->number n)))
-                                    (else acc)))))))
+      (match lst
+        (("+" n . tail) (evaluate/1/h tail (+ acc (string->number n))))
+        (("*" n . tail) (evaluate/1/h tail (* acc (string->number n))))
+        (_ acc)))))
 
 (define (evaluate/2 str)
   (match (irregex-extract "[0-9]+ \\+ [0-9]+" str)
-    ((match . _) (evaluate/2 (replace str match (number->string (apply + (map string->number (string-split match " +")))))))
+    ((match . _)
+     (evaluate/2 (replace str match (number->string (apply + (map string->number (string-split match " +")))))))
     (_ (evaluate/1 str))))
 
 (define (solve proc input)
   (define (solve/h str)
     (match (irregex-extract "\\([^)(]+\\)" str)
-      ((match . _) (solve/h (replace str match (number->string (proc (apply string-append (string-split match ")(")))))))
+      ((match . _)
+       (solve/h (replace str match (number->string (proc (apply string-append (string-split match ")(")))))))
       (_ str)))
   (print (apply + (map proc (map solve/h input)))))
 
