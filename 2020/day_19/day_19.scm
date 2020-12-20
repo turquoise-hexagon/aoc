30069872ac8b7827904843c4c14ef000e5e985ea
turquoise-hexagon <camille@lzr.pw>
2020-12-20 00:42:04 +0000

    update day_19.scm

--- 2020/day_19/day_19.scm
+++ 2020/day_19/day_19.scm
@@ -15,28 +15,29 @@
     ((rules messages)
      (list (map parse-rule rules) messages))))
 
-(define (generate-regex rules n cnt lim)
-  (let ((regex
-          (fold
-            (lambda (a acc)
-              (if (string->number a)
-                  (let ((cnt (if (string=? a n)
-                                   (+ cnt 1)
-                                   cnt)))
-                    (if (= cnt lim) acc
-                        (string-append acc (generate-regex rules a cnt lim))))
-                  (string-append acc a)))
-            (string) (cadr (assoc n rules)))))
-    (string-append "(?:" regex ")")))
+(define (generate-regex rules key lim)
+  (define (generate-regex/h key cnt lim)
+    (let ((regex (fold
+                   (lambda (a acc)
+                     (if (string->number a)
+                         (let ((cnt (if (string=? a key)
+                                        (+ cnt 1)
+                                        cnt)))
+                           (if (= cnt lim) acc
+                               (string-append acc (generate-regex/h a cnt lim))))
+                         (string-append acc a)))
+                   (string) (cadr (assoc key rules)))))
+      (string-append "(?:" regex ")")))
+  (string-append "^" (generate-regex/h key 0 lim) "$"))
 
-(define (solve rules messages n cnt lim)
-  (let ((regex (string-append "^" (generate-regex rules n cnt lim) "$")))
+(define (solve rules messages key lim)
+  (let ((regex (generate-regex rules key lim)))
     (print (length (filter (cut irregex-match? regex <>) messages)))))
 
 (let ((path (car (command-line-arguments))))
   (match (import-input path)
     ((rules messages)
-     (solve rules messages "0" 0 1)
+     (solve rules messages "0" 1)
      (let* ((rules (cons (list "8"  (list "42" "|" "42" "8"))            rules))
             (rules (cons (list "11" (list "42" "31" "|" "42" "11" "31")) rules)))
-       (solve rules messages "0" 0 5)))))
+       (solve rules messages "0" 5)))))
