eb7e8f61cc4178c237be24f1d33184982af99229
turquoise-hexagon <camille@lzr.pw>
2020-12-23 20:41:46 +0000

    fix spacing in day_07.scm

--- 2020/day_07/day_07.scm
+++ 2020/day_07/day_07.scm
@@ -29,12 +29,12 @@
   (define (solve/1/h hash)
     (let ((keys (hash-table-keys hash)))
       (hash-table-for-each input
-                           (lambda (key/i hash/i)
-                             (for-each
-                               (lambda (key)
-                                 (when (hash-table-exists? hash/i key)
-                                   (hash-table-set! hash key/i 0)))
-                               keys)))
+        (lambda (key/i hash/i)
+          (for-each
+            (lambda (key)
+              (when (hash-table-exists? hash/i key)
+                (hash-table-set! hash key/i 0)))
+            keys)))
       (if (equal? keys (hash-table-keys hash))
           keys
           (solve/1/h hash))))
@@ -46,8 +46,8 @@
       (if (null? (hash-table-keys hash))
           1
           (apply + (cons 1 (hash-table-map hash
-                                           (lambda (key hash)
-                                             (* hash (solve/2/h key)))))))))
+                             (lambda (key hash)
+                               (* hash (solve/2/h key)))))))))
   (print (- (solve/2/h color) 1)))
 
 (let ((path (car (command-line-arguments))))
