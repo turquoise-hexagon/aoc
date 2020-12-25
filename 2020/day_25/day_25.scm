508a4322c2112bfb221985da339c50875017020b
turquoise-hexagon <camille@lzr.pw>
2020-12-25 17:48:20 +0000

    use modular exponentiation instead

--- 2020/day_25/day_25.scm
+++ 2020/day_25/day_25.scm
@@ -1,28 +1,27 @@
 (import (chicken io)
         (chicken process-context)
-        (matchable)
-        (srfi 1))
+        (matchable))
 
 (define (import-input path)
   (map string->number (read-lines (open-input-file path))))
 
-(define (transform-number-once previous subject)
-  (remainder (* previous subject) 20201227))
-
-(define (transform-number subject loop)
-  (fold
-    (lambda (a acc)
-      (transform-number-once acc subject))
-    1 (iota loop)))
+(define (expt-mod b e m)
+  (let expt-mod/h ((b b) (e e) (r 1))
+    (if (= e 0)
+        r
+        (expt-mod/h (modulo (* b b) m) (quotient e 2)
+                    (if (odd? e)
+                        (modulo (* b r) m)
+                        r)))))
 
 (define (find-loop-size public)
-  (let find-loop-size/h ((loop 1) (value (transform-number-once 1 7)))
-    (if (= value public)
+  (let find-loop-size/h ((loop 0))
+    (if (= public (expt-mod 7 loop 20201227))
         loop
-        (find-loop-size/h (+ loop 1) (transform-number-once value 7)))))
+        (find-loop-size/h (+ loop 1)))))
 
 (define (solve/1 input)
-  (match input ((a b) (print (transform-number a (find-loop-size b))))))
+  (match input ((a b) (print (expt-mod b (find-loop-size a) 20201227)))))
 
 (let ((path (car (command-line-arguments))))
   (let ((input (import-input path)))
