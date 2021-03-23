0897bdaf1f02332334a79f46059b3606a76a59cb
turquoise-hexagon <camille@lzr.pw>
2021-03-23 14:58:33 +0000

    fix style in day_25

--- 2020/day_25/day_25.scm
+++ 2020/day_25/day_25.scm
@@ -8,7 +8,7 @@
   (map string->number (read-lines (open-input-file path))))
 
 (define (expt-mod b e m)
-  (let expt-mod/h ([b b] [e e] [r 1])
+  (let expt-mod/h ((b b) (e e) (r 1))
     (if (= e 0)
         r
         (expt-mod/h (modulo (* b b) m) (quotient e 2)
@@ -16,30 +16,30 @@
                         (modulo (* b r) m)
                         r)))))
 
-(define (discrete-log g h p)
-  (let ((n (+ 1 (inexact->exact (floor (sqrt p))))))
+(define (discrete-log base res mod)
+  (let ((n (inexact->exact (ceiling (sqrt mod)))))
     (let ((mem (make-hash-table)))
       (for-each
         (lambda (i)
-          (hash-table-set! mem (expt-mod g i p) i))
+          (hash-table-set! mem (expt-mod base i mod) i))
         (iota n))
-      (let ((c (expt-mod g (* n (- p 2)) p)))
+      (let ((factor (expt-mod base (* n (- mod 2)) mod)))
         (call/cc
           (lambda (return)
             (for-each
               (lambda (i)
-                (let ((t (modulo (* h (expt-mod c i p)) p)))
-                  (let ((res (hash-table-ref/default mem t #f)))
+                (let ((tmp (modulo (* res (expt-mod factor i mod)) mod)))
+                  (let ((res (hash-table-ref/default mem tmp #f)))
                     (when res (let ((ans (+ (* i n) res)))
                       (when (> ans 0) (return ans)))))))
               (iota n))
             (return -1)))))))
 
 (define (solve/1 input)
-  (let ([m 20201227])
+  (let ((m 20201227))
     (match input
       ((a b) (print (expt-mod b (discrete-log 7 a m) m))))))
 
-(let ([path (car (command-line-arguments))])
-  (let ([input (import-input path)])
+(let ((path (car (command-line-arguments))))
+  (let ((input (import-input path)))
     (solve/1 input)))
