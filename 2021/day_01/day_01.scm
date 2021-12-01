a31252e9d5fe6d673b4949d77abb3684f4b19b4d
turquoise-hexagon <camille@lzr.pw>
2021-12-01 05:09:47 +0000

    add 2021/day_01

---
+++ 2021/day_01/day_01.scm
@@ -0,0 +1,17 @@
+(import
+  (chicken io)
+  (srfi 1))
+
+(define (import-input)
+  (map string->number (read-lines)))
+
+(define (solve input)
+  (count
+    (lambda (lst)
+      (apply (cut < <> <>) lst))
+    (zip input (cdr input))))
+
+(let ((input (import-input)))
+  (print (solve input))
+  (let ((input (map (cut apply + <>) (zip input (cdr input) (cddr input)))))
+    (print (solve input))))
