3a6cd10228e5cd35cdcf7a26d7fe7536b7eb38b4
turquoise-hexagon <camille@lzr.pw>
2021-12-01 01:25:38 +0000

    rewrite 2020/day_01

--- 2020/day_01/day_01.scm
+++ 2020/day_01/day_01.scm
@@ -1,23 +1,17 @@
-(import (chicken io)
-        (chicken process-context)
-        (srfi 1))
+(import
+  (chicken io)
+  (euler)
+  (srfi 1))
 
-(define (import-input path)
-  (map string->number (read-lines (open-input-file path))))
-
-(define (combinations l n)
-  (cond ((= n 0) '(()))
-        ((null? l) '())
-        (else (append (map
-                        (lambda (a)
-                          (cons (car l) a))
-                        (combinations (cdr l) (- n 1)))
-                      (combinations (cdr l) n)))))
+(define (import-input)
+  (map string->number (read-lines)))
 
 (define (solve input n)
-  (print (apply * (car (filter (lambda (l) (= 2020 (apply + l))) (combinations input n))))))
+  (apply * (find
+             (lambda (lst)
+               (= (apply + lst) 2020))
+             (powerset input n))))
 
-(let ((path (car (command-line-arguments))))
-  (let ((input (import-input path)))
-    (solve input 2)
-    (solve input 3)))
+(let ((input (import-input)))
+  (print (solve input 2))
+  (print (solve input 3)))
