6a7177b62cf3a013d02a3c8bdae0053df265c957
turquoise-hexagon <camille@lzr.pw>
2021-12-02 05:25:46 +0000

    simplify 2021/day_02

--- 2021/day_02/day_02.scm
+++ 2021/day_02/day_02.scm
@@ -10,28 +10,25 @@
         (list (string->symbol direction) (string->number value))))
     (read-lines)))
 
-(define (solve/1 input)
-  (let ((res (foldl
-               (match-lambda*
-                 (((horizontal depth) (direction value))
-                  (case direction
-                    ((forward) (list (+ horizontal value) depth))
-                    ((down)    (list horizontal (+ depth value)))
-                    ((up)      (list horizontal (- depth value))))))
-               '(0 0) input)))
-    (apply * res)))
+(define (part/1 aim horizontal depth direction value)
+  (case direction
+    ((down)    (list aim horizontal (+ depth value)))
+    ((up)      (list aim horizontal (- depth value)))
+    ((forward) (list aim (+ horizontal value) depth))))
 
-(define (solve/2 input)
-  (let ((res (foldl
-               (match-lambda*
-                 (((aim horizontal depth) (direction value))
-                  (case direction
-                    ((down)    (list (+ aim value) horizontal depth))
-                    ((up)      (list (- aim value) horizontal depth))
-                    ((forward) (list aim (+ horizontal value) (+ depth (* aim value)))))))
-               '(0 0 0) input)))
-    (apply * (cdr res))))
+(define (part/2 aim horizontal depth direction value)
+ (case direction
+   ((down)    (list (+ aim value) horizontal depth))
+   ((up)      (list (- aim value) horizontal depth))
+   ((forward) (list aim (+ horizontal value) (+ depth (* aim value))))))
+
+(define (solve input proc)
+  (apply * (cdr (foldl
+                  (match-lambda*
+                    (((aim horizontal depth) (direction value))
+                     (proc aim horizontal depth direction value)))
+                  '(0 0 0) input))))
 
 (let ((input (import-input)))
-  (print (solve/1 input))
-  (print (solve/2 input)))
+  (print (solve input part/1))
+  (print (solve input part/2)))
