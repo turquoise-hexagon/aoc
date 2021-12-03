b6b5964ba81fe15bd9b21d15f86650b7fc7488ea
turquoise-hexagon <camille@lzr.pw>
2021-12-03 13:38:51 +0000

    simplify 2021/day_03

--- 2021/day_03/day_03.scm
+++ 2021/day_03/day_03.scm
@@ -11,14 +11,15 @@
 (define (column lst n)
   (map (cut list-ref <> n) lst))
 
-(define (gamma-and-epsil/h lst proc default)
+(define (gamma lst)
   (let ((cnt-0 (count (cut = 0 <>) lst))
         (cnt-1 (count (cut = 1 <>) lst)))
-    (if (= cnt-0 cnt-1)
-      default
-      (if (proc cnt-0 cnt-1)
-        0
-        1))))
+    (cond ((= cnt-0 cnt-1) 1)
+          ((> cnt-0 cnt-1) 0)
+          (else 1))))
+
+(define (epsil lst)
+  (if (= (gamma lst) 0) 1 0))
 
 (define (oxy-and-co2/h lst proc)
   (let loop ((lst lst) (i 0))
@@ -30,9 +31,6 @@
                   (= tmp (list-ref lst i)))
                 lst)
               (+ i 1))))))
-
-(define (gamma lst) (gamma-and-epsil/h lst > 1))
-(define (epsil lst) (gamma-and-epsil/h lst < 0))
 
 (define (oxy lst) (oxy-and-co2/h lst gamma))
 (define (co2 lst) (oxy-and-co2/h lst epsil))
