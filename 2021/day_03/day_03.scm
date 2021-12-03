77548592ed14924e5a5ed128e182ab732a8beeb0
turquoise-hexagon <camille@lzr.pw>
2021-12-03 07:40:48 +0000

    simplify 2021/day_03

--- 2021/day_03/day_03.scm
+++ 2021/day_03/day_03.scm
@@ -1,57 +1,50 @@
 (import
   (chicken io)
+  (chicken string)
+  (euler)
   (srfi 1))
 
 (define (import-input)
-  (map string->list (read-lines)))
-
-(define (invert lst)
-  (map
-    (lambda (char)
-      (case char
-        ((#\0) #\1)
-        ((#\1) #\0)))
-    lst))
+  (map (cut map string->number <>)
+    (map (cut string-chop <> 1) (read-lines))))
 
 (define (column lst n)
   (map (cut list-ref <> n) lst))
 
-(define (gamma-and-epsilon/h lst proc default)
-  (let ((cnt-0 (count (cut char=? #\0 <>) lst))
-        (cnt-1 (count (cut char=? #\1 <>) lst)))
+(define (gamma-and-epsil/h lst proc default)
+  (let ((cnt-0 (count (cut = 0 <>) lst))
+        (cnt-1 (count (cut = 1 <>) lst)))
     (if (= cnt-0 cnt-1)
       default
       (if (proc cnt-0 cnt-1)
-        #\0
-        #\1))))
+        0
+        1))))
 
-(define (  gamma lst) (gamma-and-epsilon/h lst > #\1))
-(define (epsilon lst) (gamma-and-epsilon/h lst < #\0))
+(define (oxy-and-co2/h lst proc)
+  (let oxy-and-co2/h/h ((lst lst) (i 0))
+    (if (= (length lst) 1)
+      (car lst)
+      (let ((tmp (proc (column lst i))))
+        (oxy-and-co2/h/h (filter
+                           (lambda (lst)
+                             (= tmp (list-ref lst i)))
+                           lst)
+                         (+ i 1))))))
 
-(define (oxygen-and-co2/h lst proc)
-  (foldl
-    (lambda (acc n)
-      (if (> (length acc) 1)
-        (let ((tmp (proc (column acc n))))
-          (filter
-            (lambda (lst)
-              (char=? tmp (list-ref lst n)))
-            acc))
-        acc))
-    lst (iota (length (car lst)))))
+(define (gamma lst) (gamma-and-epsil/h lst > 1))
+(define (epsil lst) (gamma-and-epsil/h lst < 0))
+
+(define (oxy lst) (oxy-and-co2/h lst gamma))
+(define (co2 lst) (oxy-and-co2/h lst epsil))
 
 (define (solve/1 input)
   (let ((cols (map (cut column input <>) (iota (length (car input))))))
-    (let ((  gamma (map   gamma cols))
-          (epsilon (map epsilon cols)))
-      (* (string->number (list->string   gamma) 2)
-         (string->number (list->string epsilon) 2)))))
+    (* (list->number (map gamma cols) 2)
+       (list->number (map epsil cols) 2))))
 
 (define (solve/2 input)
-  (let ((oxygen (car (oxygen-and-co2/h input   gamma)))
-        (   co2 (car (oxygen-and-co2/h input epsilon))))
-    (* (string->number (list->string oxygen) 2)
-       (string->number (list->string    co2) 2))))
+  (* (list->number (oxy input) 2)
+     (list->number (co2 input) 2)))
 
 (let ((input (import-input)))
   (print (solve/1 input))
