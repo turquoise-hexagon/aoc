cff1f1b9d7c72a3ffc3e68d295310bf0792e70c7
turquoise-hexagon <camille@lzr.pw>
2021-12-04 06:19:52 +0000

    simplify 2021/day_04

--- 2021/day_04/day_04.scm
+++ 2021/day_04/day_04.scm
@@ -34,17 +34,16 @@
   (receive (numbers boards) (apply values input)
     (let loop ((numbers numbers) (acc '()))
       (let ((res (find (cut has acc <>) boards)))
-        (if res (* (apply + (unmarked acc res)) (car acc))
+        (if res
+          (* (apply + (unmarked acc res)) (car acc))
           (loop (cdr numbers) (cons (car numbers) acc)))))))
 
 (define (solve/2 input)
   (receive (numbers boards) (apply values input)
     (let loop ((numbers numbers) (boards boards) (acc '()))
-      (let ((res (filter
-                   (lambda (board)
-                     (not (has acc board)))
-                   boards)))
-        (if (null? res) (* (apply + (unmarked acc (last boards))) (car acc))
+      (let ((res (remove (cut has acc <>) boards)))
+        (if (null? res)
+          (* (apply + (unmarked acc (last boards))) (car acc))
           (loop (cdr numbers) res (cons (car numbers) acc)))))))
 
 (let ((input (import-input)))
