712be60de7214570717c4a16d80309dc8d726edb
turquoise-hexagon <camille@lzr.pw>
2021-12-04 23:59:10 +0000

    rewrite 2020/day_05

--- 2020/day_05/day_05.scm
+++ 2020/day_05/day_05.scm
@@ -1,36 +1,35 @@
-(import (chicken io)
-        (chicken process-context)
-        (chicken irregex)
-        (srfi 1))
+(import
+  (chicken io)
+  (euler)
+  (srfi 1))
 
-(define (import-input path)
-  (map string->list (read-lines (open-input-file path))))
+(define (parse-pass str)
+  (map
+    (lambda (char)
+      (case char
+        ((#\F #\L) 0)
+        ((#\B #\R) 1)))
+    (string->list str)))
 
-(define (get-seat-id boarding-pass)
-  (define (get-seat/h lst)
-    (let* ((str (list->string lst))
-           (str (irregex-replace/all "[BR]" str "1"))
-           (str (irregex-replace/all "[FL]" str "0")))
-      (string->number str 2)))
-  (+ (* (get-seat/h (take boarding-pass 7)) 8)
-     (get-seat/h (drop boarding-pass 7))))
+(define (pass->id lst)
+  (receive (col row) (split-at lst 7)
+    (+ (* (list->number col 2) 8) (list->number row 2))))
 
-(define (solve/1 ids)
-  (print (apply max ids)))
+(define (import-input)
+  (map pass->id (map parse-pass (read-lines))))
 
-(define (solve/2 ids)
-  (let ((alist (map list ids)))
-    (print (call/cc
-             (lambda (return)
-               (for-each
-                 (lambda (id)
-                   (when (and (not (assoc id alist))
-                              (assoc (+ id 1) alist)
-                              (assoc (- id 1) alist))
-                     (return id)))
-                 (iota (+ (* 127 8) 7))))))))
+(define (solve/1 input)
+  (apply max input))
 
-(let ((path (car (command-line-arguments))))
-  (let ((ids (map get-seat-id (import-input path))))
-    (solve/1 ids)
-    (solve/2 ids)))
+(define (solve/2 input)
+  (let ((alist (map list input)))
+    (find
+      (lambda (id)
+        (and (assoc (+ id 1) alist)
+             (assoc (- id 1) alist)
+             (not (assoc id  alist))))
+      (iota (apply max input)))))
+
+(let ((input (import-input)))
+  (print (solve/1 input))
+  (print (solve/2 input)))
