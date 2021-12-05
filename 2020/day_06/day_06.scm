315b4fd5408261fed49190a7a6f68f92c5cbe4b8
turquoise-hexagon <camille@lzr.pw>
2021-12-05 18:05:48 +0000

    rewrite 2020/day_06

--- 2020/day_06/day_06.scm
+++ 2020/day_06/day_06.scm
@@ -1,15 +1,17 @@
-(import (chicken io)
-        (chicken process-context)
-        (chicken irregex)
-        (srfi 1))
+(import
+  (chicken io)
+  (chicken irregex)
+  (srfi 1))
 
-(define (import-input path)
-  (map (cut map string->list <>) (map (cut irregex-split "\n" <>) (irregex-split "\n\n" (read-string #f (open-input-file path))))))
+(define (import-input)
+  (map
+    (lambda (str)
+      (map string->list (irregex-split "\n" str)))
+    (irregex-split "\n{2}" (read-string #f))))
 
-(define (solve proc input)
-  (print (apply + (map length (map (cut apply proc char=? <>) input)))))
+(define (solve input proc)
+  (length (flatten (map (cut apply proc char=? <>) input))))
 
-(let ((path (car (command-line-arguments))))
-  (let ((input (import-input path)))
-    (solve lset-union input)
-    (solve lset-intersection input)))
+(let ((input (import-input)))
+  (print (solve input lset-union))
+  (print (solve input lset-intersection)))
