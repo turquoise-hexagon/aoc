1202b18ac88faaf06b5d27433a619045abf890f0
turquoise-hexagon <camille@lzr.pw>
2021-12-06 05:58:35 +0000

    simplify 2021/day_06

--- 2021/day_06/day_06.scm
+++ 2021/day_06/day_06.scm
@@ -4,31 +4,31 @@
   (srfi 1)
   (srfi 69))
 
-(define (import-input)
-  (map string->number (string-split (read-line) ",")))
-
 (define (increment! mem key value)
   (hash-table-set! mem key
     (+ (hash-table-ref/default mem key 0) value)))
+
+(define (import-input)
+  (let ((lst (map string->number (string-split (read-line) ","))))
+    (let ((mem (make-hash-table)))
+      (for-each (cut increment! mem <> 1) lst)
+      mem)))
 
 (define (iterate mem)
   (let ((next (make-hash-table)))
     (hash-table-for-each mem
       (lambda (i n)
         (for-each (cut increment! next <> n)
-          (if (= i 0)
-            '(6 8)
+          (if (= i 0) '(6 8)
             (list (- i 1))))))
     next))
 
 (define (solve input n)
-  (let ((mem (make-hash-table)))
-    (for-each (cut increment! mem <> 1) input)
-    (apply + (hash-table-values
-               (foldl
-                 (lambda (acc _)
-                   (iterate acc))
-                 mem (iota n))))))
+  (let ((res (foldl
+               (lambda (acc _)
+                 (iterate acc))
+               input (iota n))))
+    (apply + (hash-table-values res))))
 
 (let ((input (import-input)))
   (print (solve input 80))
