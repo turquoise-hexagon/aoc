44f32cba64ef2a10facfefc59d116f7777964e85
turquoise-hexagon <camille@lzr.pw>
2021-12-09 05:55:23 +0000

    add 2021/day_09

---
+++ 2021/day_09/day_09.scm
@@ -0,0 +1,59 @@
+(import
+  (chicken io)
+  (chicken string)
+  (chicken sort)
+  (euler)
+  (srfi 1)
+  (srfi 69))
+
+(define (pack-input lst)
+  (let ((mem (make-hash-table)))
+    (for-each
+      (lambda (coord)
+        (receive (x y) (apply values coord)
+          (hash-table-set! mem coord (list-ref (list-ref lst x) y))))
+      (product (iota (length lst)) (iota (length (car lst)))))
+    mem))
+
+(define (import-input)
+  (pack-input
+    (map (cut map string->number <>)
+      (map (cut string-chop <> 1) (read-lines)))))
+
+(define (neighbors mem coord)
+  (filter (cut hash-table-exists? mem <>)
+    (map (cut map + coord <>) '((1 0) (0 1) (-1 0) (0 -1)))))
+
+(define (low-point? mem coord)
+  (let ((current (hash-table-ref mem coord)))
+    (> (apply min (map (cut hash-table-ref mem <>) (neighbors mem coord))) current)))
+
+(define (bassin mem coord)
+  (let ((coords (alist->hash-table `((,coord . #t)))))
+    (let loop ((coords coords))
+      (let ((next (hash-table-copy coords)))
+        (for-each
+          (lambda (coord)
+            (let ((current (hash-table-ref mem coord)))
+              (for-each
+                (lambda (coord)
+                  (let ((tmp (hash-table-ref mem coord)))
+                    (when (> 9 tmp current)
+                      (hash-table-set! next coord #t))))
+                (neighbors mem coord))))
+          (hash-table-keys coords))
+        (if (equal? coords next)
+          coords
+          (loop next))))))
+
+(define (solve/1 input low-points)
+  (apply + (map (cut + <> 1) (map (cut hash-table-ref input <>) low-points))))
+
+(define (solve/2 input low-points)
+  (let ((lens (map hash-table-size (map (cut bassin input <>) low-points))))
+    (apply * (take (sort lens >) 3))))
+
+(let ((input (import-input)))
+  (let ((low-points (filter (cut low-point? input <>) (hash-table-keys input))))
+    (print (solve/1 input low-points))
+    (print (solve/2 input low-points))))
