dc1601203c7fb36ff4b20d70d96a8af681f334b1
turquoise-hexagon <camille@lzr.pw>
2021-12-10 11:35:07 +0000

    simplify 2021/day_10

--- 2021/day_10/day_10.scm
+++ 2021/day_10/day_10.scm
@@ -3,45 +3,47 @@
   (chicken sort)
   (srfi 1))
 
-(define openings '(#\( #\[ #\{ #\<))
-(define closings '(#\) #\] #\} #\>))
+(define openings/scores
+  '((#\( . 1)
+    (#\[ . 2)
+    (#\{ . 3)
+    (#\< . 4)))
+
+(define closings/scores
+  '((#\) . 3)
+    (#\] . 57)
+    (#\} . 1197)
+    (#\> . 25137)))
+
+(define openings (unzip1 openings/scores))
+(define closings (unzip1 closings/scores))
 
 (define matched
-  (append
-    (map cons openings closings)
-    (map cons closings openings)))
-
-(define (run lst)
-  (call/cc (lambda (_)
-             (foldl
-               (lambda (acc char)
-                 (cond ((member char openings)
-                        (cons char acc))
-                       ((char=? (cdr (assoc char matched)) (car acc))
-                        (cdr acc))
-                       (else (_ char))))
-               '() lst))))
-
-(define (import-input)
-  (partition list? (map run (map string->list (read-lines)))))
+  (append (map cons openings closings)
+          (map cons closings openings)))
 
 (define (score/1 char)
-  (case char
-    ((#\)) 3)
-    ((#\]) 57)
-    ((#\}) 1197)
-    ((#\>) 25137)))
+  (cdr (assoc char closings/scores)))
 
 (define (score/2 lst)
   (foldl
     (lambda (acc char)
-      (+ (* acc 5)
-         (case char
-           ((#\() 1)
-           ((#\[) 2)
-           ((#\{) 3)
-           ((#\<) 4))))
+      (+ (* acc 5) (cdr (assoc char openings/scores))))
     0 lst))
+
+(define (check-syntax lst)
+  (call/cc (lambda (_)
+             (foldl
+               (lambda (acc char)
+                 (if (member char openings)
+                   (cons char acc)
+                   (if (char=? (cdr (assoc char matched)) (car acc))
+                     (cdr acc)
+                     (_ char))))
+               '() lst))))
+
+(define (import-input)
+  (partition list? (map check-syntax (map string->list (read-lines)))))
 
 (define (solve/1 input)
   (apply + (map score/1 input)))
