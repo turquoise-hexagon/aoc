26a9006195fad3ab1c18085920d3d84f8246364f
turquoise-hexagon <camille@lzr.pw>
2021-12-10 04:38:45 +0000

    rewrite 2020/day_10

--- 2020/day_10/day_10.scm
+++ 2020/day_10/day_10.scm
@@ -1,40 +1,34 @@
-(import (chicken io)
-        (chicken process-context)
-        (chicken sort)
-        (srfi 1))
+(import
+  (chicken io)
+  (chicken sort)
+  (srfi 1))
 
-(define (diffs input)
-  (fold
-    (lambda (a b acc)
-      (cons (- b a) acc))
-    (list) input (cdr input)))
+(define (process-input lst)
+  (let ((lst (sort (append `(,(+ (apply max lst) 3) 0) lst) >)))
+    (map - lst (cdr lst))))
 
-(define (import-input path)
-  (let ((lst (map string->number (read-lines (open-input-file path)))))
-    (diffs (cons 0 (foldr cons (list (+ (apply max lst) 3)) (sort lst <))))))
+(define (import-input)
+  (process-input (map string->number (read-lines))))
 
 (define (tribonacci n)
   (let tribonacci/h ((a 0) (b 0) (c 1) (n n))
     (if (= n 0)
-        a
-        (tribonacci/h b c (+ a b c) (- n 1)))))
+      a
+      (tribonacci/h b c (+ a b c) (- n 1)))))
 
 (define (solve/1 input)
-  (print (* (count (cut = <> 1) input)
-            (count (cut = <> 3) input))))
+  (* (count (cut = 1 <>) input)
+     (count (cut = 3 <>) input)))
 
 (define (solve/2 input)
-  (print (fold
-           (lambda (a acc)
-             (* (tribonacci (+ a 2)) acc))
-           1 (fold
-               (lambda (a acc)
-                 (if (= a 1)
-                     (cons (+ 1 (car acc)) (cdr acc))
-                     (cons 0 acc)))
-               (list 0) input))))
+  (let ((lst (foldl
+               (lambda (acc n)
+                 (if (= n 1)
+                   (cons (+ 1 (car acc)) (cdr acc))
+                   (cons 0 acc)))
+               '(0) input)))
+    (apply * (map tribonacci (map (cut + <> 2) lst)))))
 
-(let ((path (car (command-line-arguments))))
-  (let ((input (import-input path)))
-    (solve/1 input)
-    (solve/2 input)))
+(let ((input (import-input)))
+  (print (solve/1 input))
+  (print (solve/2 input)))
