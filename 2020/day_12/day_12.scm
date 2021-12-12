1ce0dfea2f6b33cd7c713f1bba0235b251bb2f7a
turquoise-hexagon <camille@lzr.pw>
2021-12-12 07:11:45 +0000

    rewrite 2020/day_12

--- 2020/day_12/day_12.scm
+++ 2020/day_12/day_12.scm
@@ -1,51 +1,49 @@
-(import (chicken io)
-        (chicken process-context)
-        (matchable)
-        (srfi 1))
+(import
+  (chicken io)
+  (chicken string)
+  (matchable)
+  (srfi 1))
 
-(define (import-input path)
-  (map
-    (lambda (str)
-      (let ((lst (string->list str)))
-        (list (string->symbol (list->string (take lst 1)))
-              (string->number (list->string (drop lst 1))))))
-    (read-lines (open-input-file path))))
+(define (parse-instruction str)
+  (receive (head tail) (split-at (string->list str) 1)
+    `(,(string->symbol (list->string head))
+      ,(string->number (list->string tail)))))
 
 (define moveset
-  `((N . ,(lambda (position object value) (cons (+ position (* value   0+1i)) object)))
-    (S . ,(lambda (position object value) (cons (+ position (* value   0-1i)) object)))
-    (E . ,(lambda (position object value) (cons (+ position (* value   1+0i)) object)))
-    (W . ,(lambda (position object value) (cons (+ position (* value  -1+0i)) object)))
-    (F . ,(lambda (position object value) (cons (+ position (* value object)) object)))
-    (L . ,(lambda (position object value) (cons position (* object (expt 0+1i (/ value 90))))))
-    (R . ,(lambda (position object value) (cons position (/ object (expt 0+1i (/ value 90))))))))
+  `((N . ,(lambda (position object value) (list (+ position (* value   0+1i)) object)))
+    (S . ,(lambda (position object value) (list (+ position (* value   0-1i)) object)))
+    (E . ,(lambda (position object value) (list (+ position (* value   1+0i)) object)))
+    (W . ,(lambda (position object value) (list (+ position (* value  -1+0i)) object)))
+    (F . ,(lambda (position object value) (list (+ position (* value object)) object)))
+    (L . ,(lambda (position object value) (list position (* object (expt 0+1i (/ value 90))))))
+    (R . ,(lambda (position object value) (list position (/ object (expt 0+1i (/ value 90))))))))
 
-(define (solve/1 position facing input)
-  (for-each
-    (match-lambda
-      ((key value)
-       (match ((cdr (assoc key moveset)) position facing value)
-         ((a . b)
-          (set! position a)
-          (set! facing   b)))))
-    input)
-  (print (apply + (map abs (list (real-part position)
-                                 (imag-part position))))))
+(define (proc/1 lst)
+  (foldl
+    (match-lambda*
+      (((position facing) (action value))
+       ((cdr (assoc action moveset)) position facing value)))
+    '(0+0i 1+0i) lst))
 
-(define (solve/2 position waypoint input)
-  (for-each
-    (match-lambda
-      ((key value)
-       (let ((proc (cdr (assoc key moveset))))
-         (case key
-           ((N S E W) (set! waypoint (car (proc waypoint waypoint value))))
-           ((L R)     (set! waypoint (cdr (proc waypoint waypoint value))))
-           ((F)       (set! position (car (proc position waypoint value))))))))
-    input)
-  (print (apply + (map abs (list (real-part position)
-                                 (imag-part position))))))
+(define (proc/2 lst)
+  (foldl
+    (match-lambda*
+      (((position waypoint) (action value))
+       (let ((proc (cdr (assoc action moveset))))
+         (case action
+           ((N S E W) (list position (car  (proc waypoint waypoint value))))
+           ((L R)     (list position (cadr (proc waypoint waypoint value))))
+           ((F)       (list (car (proc position waypoint value)) waypoint))))))
+    '(0+0i 10+1i) lst))
 
-(let ((path (car (command-line-arguments))))
-  (let ((input (import-input path)))
-    (solve/1 0+0i  1+0i input)
-    (solve/2 0+0i 10+1i input)))
+(define (solve input proc)
+  (receive (position _) (apply values (proc input))
+    (+ (abs (real-part position))
+       (abs (imag-part position)))))
+
+(define (import-input)
+  (map parse-instruction (read-lines)))
+
+(let ((input (import-input)))
+  (print (solve input proc/1))
+  (print (solve input proc/2)))
