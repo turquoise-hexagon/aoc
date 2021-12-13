8a4c03f538e633fda3a02518bf6f2af4706ba928
turquoise-hexagon <camille@lzr.pw>
2021-12-13 09:19:51 +0000

    simplify 2021/day_13

--- 2021/day_13/day_13.scm
+++ 2021/day_13/day_13.scm
@@ -8,19 +8,17 @@
   (map string->number (irregex-split "," str)))
 
 (define (parse-fold str)
-  (let ((lst (irregex-split "[= ]" str)))
-    (receive (_ _ direction value) (apply values lst)
-      (list (string->symbol direction) (string->number value)))))
+  (receive (_ _ dimension value) (apply values (irregex-split "[= ]" str))
+    (list (string->symbol dimension) (string->number value))))
 
 (define (import-input)
-  (let ((chunks (irregex-split "\n{2}" (read-string #f))))
-    (let ((chunks (map (cut irregex-split "\n" <>) chunks)))
-      (receive (dots folds) (apply values chunks)
-        (values (map parse-dot dots) (map parse-fold folds))))))
+  (let ((lst (map (cut irregex-split "\n" <>) (irregex-split "\n{2}" (read-string #f)))))
+    (receive (dots folds) (apply values lst)
+      (values (map parse-dot dots) (map parse-fold folds)))))
 
 (define (fold-dot coord axis)
-  (receive (x y direction value) (apply values (flatten coord axis))
-    (case direction
+  (receive (x y dimension value) (apply values (flatten (list coord axis)))
+    (case dimension
       ((x) (list (min x (- (* 2 value) x)) y))
       ((y) (list x (min y (- (* 2 value) y)))))))
 
@@ -31,27 +29,25 @@
     dots folds))
 
 (define (place-dots dots)
-  (let ((mem (make-hash-table)))
-    (for-each (cut hash-table-set! mem <> #t) dots)
-    mem))
+  (let ((grid (make-hash-table)))
+    (for-each (cut hash-table-set! grid <> #t) dots)
+    grid))
 
-(define (output mem)
-  (receive (x y) (unzip2 (hash-table-keys mem))
-    (let ((x-min (apply min x))
-          (y-min (apply min y)))
-      (let ((h (- (apply max x) x-min -1))
-            (w (- (apply max y) y-min -1)))
-        (for-each
-          (lambda (y)
-            (for-each
-              (lambda (x)
-                (display (if (hash-table-exists? mem (list x y)) "#" " ")))
-              (iota h x-min))
-            (newline))
-          (iota w y-min))))))
+(define (output grid)
+  (receive (x y) (unzip2 (hash-table-keys grid))
+    (let ((h (+ (apply max x) 1))
+          (w (+ (apply max y) 1)))
+      (for-each
+        (lambda (i)
+          (for-each
+            (lambda (j)
+              (display (if (hash-table-exists? grid (list j i)) "#" " ")))
+            (iota h))
+          (newline))
+        (iota w)))))
 
 (define (solve/1 dots folds)
-  (hash-table-size (place-dots (fold-dots dots `(,(car folds))))))
+  (hash-table-size (place-dots (fold-dots dots (list (first folds))))))
 
 (define (solve/2 dots folds)
   (output (place-dots (fold-dots dots folds))))
