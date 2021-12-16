7fb9122c0733dd186dc43b88ea6dae62618c7cfe
turquoise-hexagon <camille@lzr.pw>
2021-12-16 18:15:27 +0000

    simplify 2021/day_15

--- 2021/day_15/day_15.scm
+++ 2021/day_15/day_15.scm
@@ -4,8 +4,8 @@
   (srfi 1)
   (srfi 69))
 
-(include-relative "grid.scm")
-(include-relative "queue.scm")
+(include-relative "utils/grid.scm")
+(include-relative "utils/queue.scm")
 
 (define (import-input)
   (list->grid
