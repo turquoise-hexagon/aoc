2a1c9a2cb8dd11f6e7f3be812f7c2bcce9a92c5c
turquoise-hexagon <camille@lzr.pw>
2021-12-20 13:08:56 +0000

    add debug flags to makefile

--- config.mk
+++ config.mk
@@ -1,14 +1,15 @@
-CSC      ?= csc
-CSCFLAGS ?= -O5
-
 BIN = $(shell find . -name 'day_*.scm' | sed 's|\.scm$$||g')
 
+all : CSCFLAGS += -O5
 all : $(BIN)
 
+debug : CSCFLAGS += -O0 -d3
+debug : $(BIN)
+
 % : %.scm
-	$(CSC) $(CSCFLAGS) $<
+	csc $(CSCFLAGS) $<
 
 clean :
 	rm -f $(BIN)
 
-.PHONY : all clean
+.PHONY : all debug clean
