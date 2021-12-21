32175737ca0f5503b05c20f28d47461014775faf
turquoise-hexagon <camille@lzr.pw>
2021-12-21 11:18:05 +0000

    simplify 2021/day_21

--- 2021/day_21/day_21.scm
+++ 2021/day_21/day_21.scm
@@ -22,42 +22,53 @@
   (+ (roll) (roll) (roll)))
 
 ;; part 2 rolls
+(define (counts lst)
+  (let ((acc (make-hash-table)))
+    (for-each
+      (lambda (item)
+        (hash-table-set! acc item
+          (+ (hash-table-ref/default acc item 0) 1)))
+      lst)
+    (hash-table-fold acc
+      (lambda (item count acc)
+        (cons (list item count) acc))
+      '())))
+
 (define dirac
-  (map (cut apply + <>)
-    (product '(1 2 3)
-             '(1 2 3)
-             '(1 2 3))))
+  (counts (map (cut apply + <>) (product '(1 2 3) '(1 2 3) '(1 2 3)))))
 
 (define (game/1 a b)
-  (let loop ((a a) (b b) (s/a 0) (s/b 0) (r 0))
-    (if (or (>= s/a 1000)
-            (>= s/b 1000))
-      (* (min s/a s/b) r)
+  (define (loop a b score/a score/b count)
+    (if (or (>= score/a 1000)
+            (>= score/b 1000))
+      (* (min score/a score/b) count)
       (let ((a (+ (modulo (+ a -1 (die)) 10) 1)))
-        (loop b a s/b (+ s/a a) (+ r 3))))))
+        (loop b a score/b (+ score/a a) (+ count 3)))))
+  (loop a b 0 0 0))
 
 (define (game/2 a b)
-  (let ((acc (make-hash-table)))
-    (define (loop a b s/a s/b t)
-      (let ((key (list a b s/a s/b t)))
-        (if (hash-table-exists? acc key)
-          (hash-table-ref acc key)
+  (receive (rolls counts) (unzip2 dirac)
+    (let ((acc (make-hash-table)))
+      (define (loop a b score/a score/b turn)
+        (let ((key (list a b score/a score/b turn)))
           (cond
-            ((>= s/a 21) '(1 0))
-            ((>= s/b 21) '(0 1))
+            ((hash-table-exists? acc key)
+             (hash-table-ref     acc key))
+            ((>= score/a 21) '(1 0))
+            ((>= score/b 21) '(0 1))
             (else
-              (let ((res (apply (cut map + <...>)
-                           (map
-                             (lambda (roll)
-                               (if (= t 0)
-                                 (let ((a (+ (modulo (+ a -1 roll) 10) 1)))
-                                   (loop a b (+ s/a a) s/b 1))
-                                 (let ((b (+ (modulo (+ b -1 roll) 10) 1)))
-                                   (loop a b s/a (+ s/b b) 0))))
-                             dirac))))
-                (hash-table-set! acc key res)
-                res))))))
-    (apply max (loop a b 0 0 0))))
+              (let ((result
+                      (fold
+                        (lambda (roll count acc)
+                          (map + acc
+                            (map * `(,count ,count)
+                              (if (= turn 0)
+                                (let ((a (+ (modulo (+ a -1 roll) 10) 1))) (loop a b (+ score/a a) score/b 1))
+                                (let ((b (+ (modulo (+ b -1 roll) 10) 1))) (loop a b score/a (+ score/b b) 0))))))
+                        '(0 0) rolls counts)))
+                (hash-table-set! acc key result)
+                result)))))
+      (apply max (loop a b 0 0 0)))))
 
 (receive (a b) (import-input)
   (print (game/1 a b))
