d771faba8bfdf85fa2337e604ffad010ba18cc67
turquoise-hexagon <camille@lzr.pw>
2021-12-21 12:42:11 +0000

    simplify 2021/day_21

--- 2021/day_21/day_21.scm
+++ 2021/day_21/day_21.scm
@@ -45,26 +45,25 @@
 (define (game/2 a b)
   (receive (rolls counts) (dirac)
     (let ((acc (make-hash-table)))
-      (define (loop a b score/a score/b turn)
-        (let ((key (list a b score/a score/b turn)))
+      (define (loop a b score/a score/b)
+        (let ((key (list a b score/a score/b)))
           (cond
             ((hash-table-exists? acc key)
              (hash-table-ref     acc key))
-            ((>= score/a 21) '(1 0))
             ((>= score/b 21) '(0 1))
             (else
-              (let ((result
-                      (fold
-                        (lambda (roll count acc)
-                          (map + acc
-                            (map * `(,count ,count)
-                              (if (= turn 0)
-                                (let ((a (+ (modulo (+ a -1 roll) 10) 1))) (loop a b (+ score/a a) score/b 1))
-                                (let ((b (+ (modulo (+ b -1 roll) 10) 1))) (loop a b score/a (+ score/b b) 0))))))
-                        '(0 0) rolls counts)))
-                (hash-table-set! acc key result)
-                result)))))
-      (apply max (loop a b 0 0 0)))))
+              (let ((res
+                      (reverse
+                        (fold
+                          (lambda (roll count acc)
+                            (map + acc
+                              (map * `(,count ,count)
+                                (let ((a (+ (modulo (+ a -1 roll) 10) 1)))
+                                  (loop b a score/b (+ score/a a))))))
+                          '(0 0) rolls counts))))
+                (hash-table-set! acc key res)
+                res)))))
+      (apply max (loop a b 0 0)))))
 
 (receive (a b) (import-input)
   (print (game/1 a b))
