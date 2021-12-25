fc5409579eedb28fa7c86dcbd896b82ed710f1ba
turquoise-hexagon <camille@lzr.pw>
2021-12-25 15:00:24 +0000

    simplify 2021/day_09

--- 2021/day_09/day_09.scm
+++ 2021/day_09/day_09.scm
@@ -6,45 +6,50 @@
   (srfi 1)
   (srfi 69))
 
-(define (pack-input lst)
-  (let ((mem (make-hash-table)))
-    (for-each
-      (lambda (coord)
-        (receive (x y) (apply values coord)
-          (hash-table-set! mem coord (list-ref (list-ref lst x) y))))
-      (product (iota (length lst)) (iota (length (car lst)))))
-    mem))
-
 (define (import-input)
-  (pack-input
+  (list->array
     (map (cut map string->number <>)
-      (map (cut string-chop <> 1) (read-lines)))))
+      (map (cut string-chop <> 1)
+        (read-lines)))))
 
-(define (neighbors mem coord)
-  (filter (cut hash-table-exists? mem <>)
-    (map (cut map + coord <>) '((1 0) (0 1) (-1 0) (0 -1)))))
+(define (neighbors array coord)
+  (filter (cut array-exists? array <>)
+    (map (cut map + <> coord)
+      '((1 0) (0 1) (-1 0) (0 -1)))))
 
-(define (low-point? mem coord)
-  (> (apply min (map (cut hash-table-ref mem <>) (neighbors mem coord)))
-     (hash-table-ref mem coord)))
+(define (low-point? array coord)
+  (> (apply min
+       (map (cut array-ref array <>)
+         (neighbors array coord)))
+     (array-ref array coord)))
 
-(define (bassin mem coord)
+(define (low-points array)
+  (receive (h w) (apply values (array-dimensions array))
+    (filter (cut low-point? array <>)
+      (product
+        (range 0 (- h 1))
+        (range 0 (- w 1))))))
+
+(define (bassin array coord)
   (let ((acc (make-hash-table)))
     (let loop ((coord coord))
       (unless (hash-table-exists? acc coord)
-        (unless (= (hash-table-ref mem coord) 9)
+        (unless (= (array-ref array coord) 9)
           (hash-table-set! acc coord #t)
-          (for-each loop (neighbors mem coord)))))
+          (for-each loop (neighbors array coord)))))
     acc))
 
 (define (solve/1 input low-points)
-  (apply + (map (cut + <> 1) (map (cut hash-table-ref input <>) low-points))))
+  (apply +
+    (map (cut + <> 1)
+      (map (cut array-ref input <>)
+        low-points))))
 
 (define (solve/2 input low-points)
   (let ((lens (map hash-table-size (map (cut bassin input <>) low-points))))
     (apply * (take (sort lens >) 3))))
 
 (let ((input (import-input)))
-  (let ((low-points (filter (cut low-point? input <>) (hash-table-keys input))))
+  (let ((low-points (low-points input)))
     (print (solve/1 input low-points))
     (print (solve/2 input low-points))))
