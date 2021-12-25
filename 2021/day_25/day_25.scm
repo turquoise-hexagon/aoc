591ece89f92098d4eebb84c6dc61309672dc0970
turquoise-hexagon <camille@lzr.pw>
2021-12-25 17:21:59 +0000

    simplify 2021/day_25

--- 2021/day_25/day_25.scm
+++ 2021/day_25/day_25.scm
@@ -17,17 +17,16 @@
     (if (char=? (array-ref array next) #\.) next #f)))
 
 (define (iterate array type)
-  (receive (h w) (apply values (array-dimensions array))
-    (let ((acc (array-copy array)))
-      (for-each
-        (lambda (coord)
-          (when (char=? (array-ref array coord) type)
-            (let ((next (neighbor array coord type)))
-              (when next
-                (array-set! acc coord #\.)
-                (array-set! acc next type)))))
-        (array-indexes array))
-      acc)))
+  (let ((acc (array-copy array)))
+    (for-each
+      (lambda (coord)
+        (when (char=? (array-ref array coord) type)
+          (let ((next (neighbor array coord type)))
+            (when next
+              (array-set! acc coord #\.)
+              (array-set! acc next type)))))
+      (array-indexes array))
+    acc))
 
 (define (solve input)
   (let loop ((i 1) (acc input))
