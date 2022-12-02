1215896cf53e7feca9519dd36f046b9e261ce5da
turquoise-hexagon <camille@lzr.pw>
2022-12-02 21:47:58 +0000

    update 2021/02

--- 2021/02/solution.scm
+++ 2021/02/solution.scm
@@ -1,26 +1,30 @@
 (import
   (chicken io)
-  (chicken string)
-  (matchable))
+  (chicken string))
 
 (define (parse-command str)
-  (receive (direction value) (apply values (string-split str " "))
-    (list (string->symbol direction) (string->number value))))
+  (apply
+    (lambda (val dir)
+      (delay
+        (values
+          (string->symbol val)
+          (string->number dir))))
+    (string-split str " ")))
 
 (define (import-input)
   (map parse-command (read-lines)))
 
 (define (solve input)
-  (foldl
-    (match-lambda*
-      (((position depth/1 depth/2) (direction value))
-       (case direction
-         ((down)    (list position (+ depth/1 value) depth/2))
-         ((up)      (list position (- depth/1 value) depth/2))
-         ((forward) (list (+ position value) depth/1 (+ depth/2 (* depth/1 value)))))))
-    '(0 0 0) input))
+  (let loop ((lst input) (pos 0) (depth/1 0) (depth/2 0))
+    (if (null? lst)
+      (list
+        (* pos depth/1)
+        (* pos depth/2))
+      (let-values (((dir val) (force (car lst))))
+        (case dir
+          ((up)      (loop (cdr lst) pos (- depth/1 val) depth/2))
+          ((down)    (loop (cdr lst) pos (+ depth/1 val) depth/2))
+          ((forward) (loop (cdr lst) (+ pos val) depth/1 (+ depth/2 (* depth/1 val)))))))))
 
 (let ((input (import-input)))
-  (receive (position depth/1 depth/2) (apply values (solve input))
-    (print (* position depth/1))
-    (print (* position depth/2))))
+  (for-each print (solve input)))
