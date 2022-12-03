69b6c9d5dc376270e1870cefefc70290ad67a949
turquoise-hexagon <camille@lzr.pw>
2022-12-03 05:29:47 +0000

    add 2022/03

---
+++ 2022/03/solution.scm
@@ -0,0 +1,33 @@
+(import
+  (chicken io)
+  (srfi 1))
+
+(define (import-input)
+  (map
+    (lambda (_)
+      (chop _ (/ (length _) 2)))
+    (map string->list (read-lines))))
+
+(define (priority sack)
+  (let ((_ (car sack)))
+    (if (char-lower-case? _)
+      (+ (- (char->integer _) (char->integer #\a)) 1)
+      (+ (- (char->integer _) (char->integer #\A)) 27))))
+
+(define (solve/1 input)
+  (apply +
+    (map
+      (lambda (sack)
+        (priority (apply lset-intersection char=? sack)))
+      input)))
+
+(define (solve/2 input)
+  (apply +
+    (map
+      (lambda (group)
+        (priority (apply lset-intersection char=? (map flatten group))))
+      (chop input 3))))
+
+(let ((input (import-input)))
+  (print (solve/1 input))
+  (print (solve/2 input)))
