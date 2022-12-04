47974112160d5d46ee6420160b83d5d96c1ccca5
turquoise-hexagon <camille@lzr.pw>
2022-12-04 05:19:35 +0000

    add 2022/04

---
+++ 2022/04/solution.scm
@@ -0,0 +1,32 @@
+(import
+  (chicken io)
+  (chicken string)
+  (srfi 1))
+
+(define (import-input)
+  (map
+    (lambda (_)
+      (map string->number (string-split _ "-,")))
+    (read-lines)))
+
+(define (valid?/1 lst)
+  (apply
+    (lambda (a b c d)
+      (or (<= a c d b)
+          (<= c a b d)))
+    lst))
+
+(define (valid?/2 lst)
+  (apply
+    (lambda (a b c d)
+      (or (<= a c b d)
+          (<= c a d b)
+          (valid?/1 lst)))
+    lst))
+
+(define (solve input proc)
+  (count proc input))
+
+(let ((input (import-input)))
+  (print (solve input valid?/1))
+  (print (solve input valid?/2)))
