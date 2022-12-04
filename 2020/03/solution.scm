c575a4c45158b77dcfb513d98845ff19a93ed936
turquoise-hexagon <camille@lzr.pw>
2022-12-04 04:45:05 +0000

    update 2020/03

--- 2020/03/solution.scm
+++ 2020/03/solution.scm
@@ -4,17 +4,21 @@
 (define (import-input)
   (map string->list (read-lines)))
 
-(define (encountered-trees input y-o x-o)
-  (let ((h (length input)) (w (length (car input))))
-    (do ((x 0 (+ x x-o))
-         (y 0 (+ y y-o))
-         (t 0 (+ t (if (char=? (list-ref (list-ref input x) (modulo y w)) #\#)
-                     1
-                     0))))
-      ((>= x h) t))))
+(define (encountered-trees input w h y x)
+  (do ((i 0 (+ i x))
+       (j 0 (+ j y))
+       (acc 0 (if (char=? (list-ref (list-ref input i) (modulo j w)) #\#)
+                (+ acc 1)
+                acc)))
+    ((>= i h) acc)))
 
 (define (solve input offsets)
-  (apply * (map (cut apply encountered-trees input <>) offsets)))
+  (let ((h (length input)) (w (length (car input))))
+    (apply *
+      (map
+        (lambda (offset)
+          (apply encountered-trees input w h offset))
+        offsets))))
 
 (let ((input (import-input)))
   (print (solve input '((3 1))))
