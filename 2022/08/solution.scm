67bde4856a8e5c47175e5338aa05e1b31a6eadc1
turquoise-hexagon <camille@lzr.pw>
2022-12-08 19:35:41 +0000

    update 2022/08

--- 2022/08/solution.scm
+++ 2022/08/solution.scm
@@ -10,58 +10,64 @@
     ( 1  0)
     ( 0 -1)))
 
-(define (neighbors array coord)
-  (map
-    (lambda (offset)
-      (let loop ((coord coord) (acc '()))
-        (let ((_ (map + coord offset)))
-          (if (array-exists? array _)
-            (loop _ (cons _ acc))
-            (reverse acc)))))
-    offsets))
-
 (define (import-input)
   (list->array
     (map
-      (lambda (_)
-        (map string->number (string-chop _ 1)))
+      (lambda (str)
+        (map string->number (string-chop str 1)))
       (read-lines))))
+
+(define (_neighbors array coord offset)
+  (let loop ((coord coord) (acc '()))
+    (let ((_ (map + coord offset)))
+      (if (array-exists? array _)
+        (loop _ (cons _ acc))
+        (reverse acc)))))
+
+(define (neighbors array coord)
+  (map
+    (lambda (offset)
+      (_neighbors array coord offset))
+    offsets))
 
 (define (visible? array coord)
   (let ((value (array-ref array coord)))
     (any
       (lambda (coords)
         (every
-          (lambda (_)
-            (> value (array-ref array _)))
+          (lambda (coord)
+            (> value (array-ref array coord)))
           coords))
       (neighbors array coord))))
+
+(define (_scenic-score array value coords)
+  (let loop ((lst coords) (acc 0))
+    (if (null? lst)
+      acc
+      (let ((_ (+ acc 1)))
+        (if (> value (array-ref array (car lst)))
+          (loop (cdr lst) _)
+          _)))))
 
 (define (scenic-score array coord)
   (let ((value (array-ref array coord)))
     (apply *
       (map
         (lambda (coords)
-          (let loop ((lst coords) (acc 0))
-            (if (null? lst)
-              acc
-              (let ((_ (+ acc 1)))
-                (if (> value (array-ref array (car lst)))
-                  (loop (cdr lst) _)
-                  _)))))
+          (_scenic-score array value coords))
         (neighbors array coord)))))
 
 (define (solve/1 input)
   (count
-    (lambda (_)
-      (visible? input _))
+    (lambda (coord)
+      (visible? input coord))
     (array-indexes input)))
 
 (define (solve/2 input)
   (apply max
     (map
-      (lambda (_)
-        (scenic-score input _))
+      (lambda (coord)
+        (scenic-score input coord))
       (array-indexes input))))
 
 (let ((input (import-input)))
