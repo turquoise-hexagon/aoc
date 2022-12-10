822686c7cfc08b5fb8ac4dd43834e1e8211dfb3e
turquoise-hexagon <camille@lzr.pw>
2022-12-10 07:28:37 +0000

    update 2022/10

--- 2022/10/solution.scm
+++ 2022/10/solution.scm
@@ -8,28 +8,28 @@
   (reverse
     (foldl
       (lambda (acc str)
-        (let ((_ (car acc)))
+        (let ((x (car acc)))
           (match (string-split str " ")
             (("addx" n)
-             (cons (+ _ (string->number n)) (cons _ acc)))
+             (cons (+ x (string->number n)) (cons x acc)))
             (("noop")
-             (cons _ acc)))))
+             (cons x acc)))))
       '(1) (read-lines))))
 
 (define (solve/1 input)
   (apply +
     (map
-      (lambda (_)
-        (* _ (list-ref input (- _ 1))))
+      (lambda (i)
+        (* i (list-ref input (- i 1))))
       (range 20 220 40))))
 
 (define (solve/2 input)
   (for-each
-    (lambda (x _)
-      (if (<= (abs (- x (modulo (- _ 1) 40))) 1)
+    (lambda (x i)
+      (if (<= (abs (- x (modulo (- i 1) 40))) 1)
         (display "#")
         (display " "))
-      (if (= (modulo _ 40) 0)
+      (if (= (modulo i 40) 0)
         (newline)))
     (butlast input) (range 1 (length input))))
 
