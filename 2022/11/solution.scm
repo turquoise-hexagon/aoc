7ad1c9df0be8be822f123f8ba0b02e9d6e19a540
turquoise-hexagon <camille@lzr.pw>
2022-12-11 14:32:54 +0000

    update 2022/11

--- 2022/11/solution.scm
+++ 2022/11/solution.scm
@@ -4,41 +4,32 @@
   (chicken sort)
   (srfi 1))
 
+(define regex ".+\n.+: ([0-9, ]+)\n.+ ([*+]) ([0-9]+|old)\n.+ ([0-9]+)\n.+ ([0-9]+)\n.+ ([0-9]+)\n?")
+
 (define-record monkey start lst operation test a b)
-
-(define (parse-start lst)
-  (filter-map string->number lst))
-
-(define (parse-operation lst)
-  (apply
-    (lambda (_ _ _ _ operator value)
-      (let ((operator
-              (case (string->symbol operator)
-                ((+) +)
-                ((*) *)))
-            (value (string->number value)))
-        (lambda (i) (operator i (if value value i)))))
-    lst))
-
-(define (parse-others lst)
-  (string->number (last lst)))
 
 (define (parse-chunk chunk)
   (apply
-    (lambda (_ start operation test a b)
+    (lambda (start operator value test a b)
       (make-monkey
-        (parse-start start) '()
-        (parse-operation operation)
-        (parse-others test)
-        (parse-others a)
-        (parse-others b)))
-    (map
-      (lambda (str)
-        (irregex-split "[, ]" str))
-      (irregex-split "\n" chunk))))
+        (map string->number (irregex-split ", " start)) '()
+        (let ((operator
+                (case (string->symbol operator)
+                  ((*) *)
+                  ((+) +)))
+              (value (string->number value)))
+          (lambda (i) (operator i (if value value i))))
+        (string->number test)
+        (string->number a)
+        (string->number b)))
+    (let ((matches (irregex-match regex chunk)))
+      (map
+        (lambda (index)
+          (irregex-match-substring matches index))
+        (iota (irregex-match-num-submatches matches) 1)))))
 
 (define (import-input)
-  (map parse-chunk (irregex-split "\n{2}" (read-string))))
+  (map parse-chunk (irregex-split "\n\n" (read-string))))
 
 (define (monkey-reset! monkey)
   (monkey-lst-set! monkey (monkey-start monkey)))
@@ -52,18 +43,11 @@
                  (if (= (modulo i (monkey-test monkey)) 0)
                    (monkey-a monkey)
                    (monkey-b monkey)))))
-        (monkey-lst-set! target
-          (cons i (monkey-lst target)))))
+        (monkey-lst-set! target (cons i (monkey-lst target)))))
     (monkey-lst monkey))
   (let ((result (length (monkey-lst monkey))))
     (monkey-lst-set! monkey '())
     result))
-
-(define (monkey-iterate!/all monkeys relief magic)
-  (map
-    (lambda (monkey)
-      (monkey-iterate! monkey monkeys relief magic))
-    monkeys))
 
 (define (solve . arguments)
   (define (_solve input iterations relief)
@@ -71,7 +55,11 @@
     (let ((magic (foldl * relief (map monkey-test input))))
       (foldl
         (lambda (acc _)
-          (map + acc (monkey-iterate!/all input relief magic)))
+          (map + acc
+            (map
+              (lambda (monkey)
+                (monkey-iterate! monkey input relief magic))
+              input)))
         (make-list (length input) 0) (iota iterations))))
   (apply * (take (sort (apply _solve arguments) >) 2)))
 
