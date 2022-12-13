df85454d94c60b5572f424b2fa76fd4dd719be37
turquoise-hexagon <camille@lzr.pw>
2022-12-13 06:07:50 +0000

    add 2022/13

---
+++ 2022/13/solution.scm
@@ -0,0 +1,61 @@
+(import
+  (chicken io)
+  (chicken irregex)
+  (chicken port)
+  (chicken sort)
+  (chicken string)
+  (srfi 1))
+
+(define (internalize str)
+  (let ((_ (string-append "'" (string-translate str "[]," "() "))))
+    (eval (call-with-input-string _ read))))
+
+(define (import-input)
+  (map
+    (lambda (_)
+      (map internalize (irregex-split "\n" _)))
+    (irregex-split "\n\n" (read-string))))
+
+(define (order l r)
+  (cond
+    ((and (integer? l)
+          (integer? r))
+     (cond ((> l r) #f)
+           ((= l r) '_)
+           ((< l r) #t)))
+    ((and (null? l)
+          (null? r))
+     2)
+    ((null? l) #t)
+    ((null? r) #f)
+    ((and (list? l)
+          (list? r))
+     (let ((_ (order (car l)
+                     (car r))))
+       (if (boolean? _)
+         _
+         (order (cdr l)
+                (cdr r)))))
+    ((integer? l) (order (list l) r))
+    ((integer? r) (order l (list r)))))
+
+(define (solve/1 input)
+  (fold
+    (lambda (lst index acc)
+      (if (apply order lst)
+        (+ acc index)
+        acc))
+    0 input (iota (length input) 1)))
+
+(define (solve/2 input)
+  (let* ((separators '(((2)) ((6)))) (result (sort (delete-duplicates (join input separators) equal?) order)))
+    (fold
+      (lambda (lst index acc)
+        (if (member lst separators)
+          (* acc index)
+          acc))
+      1 result (iota (length result) 1))))
+
+(let ((input (import-input)))
+  (print (solve/1 input))
+  (print (solve/2 input)))
