ae5d01184815ac3c2c71afa7b13c05a27cd4200a
turquoise-hexagon <camille@lzr.pw>
2022-12-15 18:59:12 +0000

    update 2022/15

--- 2022/15/solution.scm
+++ 2022/15/solution.scm
@@ -11,67 +11,72 @@
   (apply + (map abs (map - a b))))
 
 (define (parse-sensor str)
-  (let ((_ (irregex-extract "-?[0-9]+" str)))
-    (match (chop (map string->number _) 2)
-      ((a b)
-       (list a (distance a b))))))
+  (match
+    (let ((_ (irregex-extract "-?[0-9]+" str)))
+      (chop (map string->number _) 2))
+    ((a b)
+     (list a (distance a b)))))
 
 (define (import-input)
   (map parse-sensor (read-lines)))
 
+(define (_ranges lst n)
+  (match lst
+    (((x y) r)
+     (let ((_ (- r (abs (- y n)))))
+       (if (> _ 0)
+         (list
+           (- x _)
+           (+ x _))
+         #f)))))
+
+(define (ranges lst n)
+  (sort
+    (filter-map
+      (lambda (_)
+        (_ranges _ n))
+      lst)
+    (lambda (a b)
+      (< (car a)
+         (car b)))))
+
+(define (find-spaces lst n)
+  (let ((_ (ranges lst n)))
+    (foldl
+      (lambda (acc next)
+        (match-let*
+          (((head . tail) acc)
+           ((a b) head)
+           ((c d) next))
+          (let ((_ (list a (max b d))))
+            (if (>= b c)
+              (cons _ tail)
+              (cons head acc)))))
+      (list (car _)) (cdr _))))
+
 (define (solve/1 input n)
-  (define process-sensor
-    (match-lambda
-      (((x y) r)
-       (let ((_ (- r (abs (- y n)))))
-         (if (> _ 0)
-           (list
-             (- x _)
-             (+ x _))
-           #f)))))
-
-  (define (generate-ranges)
-    (sort (filter-map process-sensor input)
-      (lambda (a b)
-        (< (car a)
-           (car b)))))
-
-  (define (find-spaces)
-    (let ((_ (generate-ranges)))
-      (foldl
-        (lambda (acc next)
-          (match-let*
-            (((head . tail) acc)
-             ((a b) head)
-             ((c d) next))
-            (let ((temp (list a (max b d))))
-              (if (>= b c)
-                (cons temp tail)
-                (cons head acc)))))
-        (list (car _)) (cdr _))))
-
   (apply +
     (map
       (lambda (_)
         (apply - (reverse _)))
-      (find-spaces))))
+      (find-spaces input n))))
+
+(define (in-bound? point bound)
+  (every
+    (lambda (_)
+      (<= 0 _ bound))
+    point))
+
+(define (in-range? point table)
+  (every
+    (lambda (_)
+      (> (distance _ point) (hash-table-ref table _)))
+    (hash-table-keys table)))
 
 (define (solve/2 input n)
   (let ((t (make-hash-table))
         (a (make-hash-table))
         (b (make-hash-table)))
-
-    (define (in-bound? point)
-      (every
-        (lambda (_)
-          (<= 0 _ n))
-        point))
-
-    (define (in-range? point)
-      (every
-        (lambda (_)
-          (> (distance _ point) (hash-table-ref t _)))
-        (hash-table-keys t)))
 
     (for-each
       (lambda (_)
@@ -94,8 +99,8 @@
            ((x (quotient (- b a) 2))
             (y (quotient (+ b a) 2))
             (p (list x y)))
-           (if (and (in-bound? p)
-                    (in-range? p))
+           (if (and (in-bound? p n)
+                    (in-range? p t))
              (+ (* x n) y)
              (loop tail))))))))
 
