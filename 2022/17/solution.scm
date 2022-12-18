ee4e3b6f14a02b0658ea55957285ddc817341e4c
turquoise-hexagon <camille@lzr.pw>
2022-12-18 11:05:37 +0000

    update 2022/17

--- 2022/17/solution.scm
+++ 2022/17/solution.scm
@@ -47,81 +47,84 @@
           (char=? (array-ref array coord) #\#))
         (array-indexes array)))))
 
-(define (valid-coords? content coords)
+(define (valid-coords? mem coords)
   (every
     (lambda (coord)
-      (and (not (hash-table-exists? content coord)) (<= 0 (second coord) (- WIDTH 1))))
+      (and (not (hash-table-exists? mem coord)) (<= 0 (second coord) (- WIDTH 1))))
     coords))
 
-(define (move-rock content coords move)
+(define (move-rock mem coords move)
   (let ((acc (map
                (lambda (coord)
                  (map + coord move))
                coords)))
-    (if (valid-coords? content acc)
+    (if (valid-coords? mem acc)
       acc
       coords)))
 
-(define (identifier content current-height moves-index rocks-index)
-  (list moves-index rocks-index
+(define (identifier mem h moves-i rocks-i)
+  (list moves-i rocks-i
     (filter
       (lambda (coord)
-        (hash-table-exists? content (map + coord (list current-height 0))))
+        (hash-table-exists? mem (map + coord (list h 0))))
       (product
         (range 0 (- SECTION 1))
         (range 0 (- WIDTH   1))))))
 
-(define (solve moves rocks iterations)
-  (let ((cache (make-hash-table)) (content (make-hash-table)))
+(define (solve moves rocks n)
+  (let ((cache (make-hash-table)) (mem (make-hash-table)))
 
     ;; add a bottom to the tower
     (for-each
       (lambda (i)
-        (hash-table-set! content (list 0 i) #t))
+        (hash-table-set! mem (list 0 i) #t))
       (range 0 (- WIDTH 1)))
 
-    (let loop ((i 0) (current-height 0) (uncycled-height 0) (moves-index 0) (rocks-index 0))
-      (if (= i iterations)
-        (abs (+ current-height uncycled-height))
+    (let loop ((i 0) (h 0) (added-h 0) (moves-i 0) (rocks-i 0))
+      (if (= i n)
+        (abs (+ h added-h))
 
         ;; get a fresh rock
         (let*
-          ((rock (list-ref rocks rocks-index))
+          ((rock (list-ref rocks rocks-i))
            (height (rock-height rock))
            (coords (rock-coords rock))
-           (coords (move-rock content coords (list (- current-height 3 height) OFFSET))))
+           (coords (move-rock mem coords (list (- h 3 height) OFFSET))))
 
-          (let subloop ((coords coords) (moves-index moves-index))
+          (let subloop ((coords coords) (moves-i moves-i))
             ;; move the rock
             (let*
-              ((move (list-ref moves moves-index))
-               (push (move-rock content coords move))
-               (grav (move-rock content push '(1 0)))
-               (moves-index (modulo (+ moves-index 1) (length moves))))
+              ((move (list-ref moves moves-i))
+               (push (move-rock mem coords move))
+               (grav (move-rock mem push '(1 0)))
+               (moves-i (modulo (+ moves-i 1) (length moves))))
               (if (equal? push grav)
 
-                ;; rock settles
+                ;; settle the rock
                 (begin
                   (for-each
                     (lambda (coord)
-                      (hash-table-set! content coord #t))
+                      (hash-table-set! mem coord #t))
                     push)
-                  (let* ((current-height (min current-height (apply min (map first push)))) (id (identifier content current-height moves-index rocks-index)))
+                  (let* ((h (min h (apply min (map first push)))) (id (identifier mem h moves-i rocks-i)))
 
-                    ;; jump ahread
                     (if (hash-table-exists? cache id)
+                      ;; jump ahead
                       (apply
-                        (lambda (old-i old-height)
-                          (let* ((diff-i (- i old-i)) (diff-height (- current-height old-height)) (mul (quotient (- iterations i) diff-i)))
-                            (loop (+ i (* mul diff-i) 1) current-height (+ uncycled-height (* mul diff-height)) moves-index
-                              (modulo (+ rocks-index 1) (length rocks)))))
+                        (lambda (old-i old-h)
+                          (let*
+                            ((diff-i (- i old-i))
+                             (diff-h (- h old-h))
+                             (mul (quotient (- n i) diff-i)))
+                            (loop (+ i (* mul diff-i) 1) h (+ added-h (* mul diff-h)) moves-i
+                              (modulo (+ rocks-i 1) (length rocks)))))
                         (hash-table-ref cache id))
 
                       (begin
-                        (hash-table-set! cache id (list i current-height))
-                        (loop (+ i 1) current-height uncycled-height moves-index
-                          (modulo (+ rocks-index 1) (length rocks)))))))
-                (subloop grav moves-index)))))))))
+                        (hash-table-set! cache id (list i h))
+                        (loop (+ i 1) h added-h moves-i
+                          (modulo (+ rocks-i 1) (length rocks)))))))
+                (subloop grav moves-i)))))))))
 
 (let ((moves (import-input)) (rocks (map parse-rock ROCKS)))
   (print (solve moves rocks 2022))
