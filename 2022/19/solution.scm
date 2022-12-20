5cda314b0c3ad3b9ec14d8ee167d536a6e437331
turquoise-hexagon <camille@lzr.pw>
2022-12-20 12:11:20 +0000

    update 2022/19

--- 2022/19/solution.scm
+++ 2022/19/solution.scm
@@ -2,9 +2,28 @@
   (chicken io)
   (chicken string)
   (euler)
-  (srfi 1))
+  (srfi 1)
+  (srfi 69))
 
-(define MAGIC 10000)
+(define MAGIC 3000)
+
+;; priority-queue helpers
+(define (priority-queue-take comp? queue n)
+  (let loop ((i 0) (queue queue) (acc priority-queue-empty))
+    (if (priority-queue-empty? queue)
+      acc
+      (if (= i n)
+        acc
+        (loop (+ i 1) (priority-queue-rest comp? queue)
+          (let ((_ (priority-queue-first queue)))
+            (priority-queue-insert comp? _ acc)))))))
+
+(define (priority-queue-fold comp? queue proc acc)
+  (let loop ((queue queue) (acc acc))
+    (if (priority-queue-empty? queue)
+      acc
+      (loop (priority-queue-rest comp? queue)
+        (proc acc (priority-queue-first queue))))))
 
 (define (parse-blueprint str)
   (apply
@@ -37,39 +56,35 @@
     (third b)
     (third a)))
 
-(define (priority-queue-take comp? queue n)
-  (let loop ((i 0) (queue queue) (acc priority-queue-empty))
-    (if (priority-queue-empty? queue)
-      acc
-      (if (= i n)
-        acc
-        (loop (+ i 1) (priority-queue-rest comp? queue)
-          (let ((_ (priority-queue-first queue)))
-            (priority-queue-insert comp? _ acc)))))))
-
 (define (search lst n)
-  (priority-queue-first
-    (foldl
-      (lambda (acc _)
-        (priority-queue-take comp?
-          (foldl
-            (lambda (acc _)
-              (apply
-                (lambda (resources robots total)
-                  (foldl
-                    (lambda (acc _)
-                      (apply
-                        (lambda (cost addition)
-                          (if (every >= resources cost)
-                            (let ((_ (list (map + resources (map - robots cost)) (map + robots addition) (map + robots total))))
-                              (priority-queue-insert comp? _ acc))
-                            acc))
-                        _))
-                    acc lst))
-                _))
-            priority-queue-empty (priority-queue->list comp? acc))
-          MAGIC))
-      (priority-queue-insert comp? '((0 0 0 0) (0 0 0 1) (0 0 0 0)) priority-queue-empty) (iota n))))
+  (let ((cache (make-hash-table)))
+    (priority-queue-first
+      (foldl
+        (lambda (acc _)
+          (priority-queue-take comp?
+            (priority-queue-fold comp? acc
+              (lambda (acc _)
+                (apply
+                  (lambda (resources robots total)
+                    (foldl
+                      (lambda (acc _)
+                        (apply
+                          (lambda (cost addition)
+                            (if (every >= resources cost)
+                              (let* ((_ (list (map + resources (map - robots cost)) (map + robots addition) (map + robots total))) (id (list->number (flatten _))))
+                                (if (hash-table-exists? cache id)
+                                  acc
+                                  (begin
+                                    (hash-table-set! cache id #t)
+                                    (priority-queue-insert comp? _ acc))))
+                              acc))
+                          _))
+                      acc lst))
+                  _))
+              priority-queue-empty)
+            MAGIC))
+        (priority-queue-insert comp? '((0 0 0 0) (0 0 0 1) (0 0 0 0)) priority-queue-empty)
+        (iota n)))))
 
 (define (solve/1 input)
   (apply +
