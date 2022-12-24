8241cec1a0a168562e7d3c14a774059a9ce2da02
turquoise-hexagon <camille@lzr.pw>
2022-12-24 13:52:47 +0000

    update 2022/24

--- 2022/24/solution.scm
+++ 2022/24/solution.scm
@@ -4,47 +4,45 @@
   (srfi 1)
   (srfi 69))
 
-(define MAGIC 1000)
-
-(define OFFSETS
+(define offsets
   '((-1  0)
     ( 0  1)
     ( 1  0)
     ( 0 -1)
     ( 0  0)))
 
-(define (iterate! table coord offset dims)
+(define (iterate-wind! table coord offset dimensions)
   (hash-table-set! table coord #t)
-  (list
-    (map
-      (lambda (c o d)
-        (modulo (+ c o d) d))
-      coord offset dims)
-    offset dims))
+  (list (map
+          (lambda (c o d)
+            (modulo (+ c o d) d))
+          coord offset dimensions)
+    offset dimensions))
 
-(define (generate-cache lst)
-  (let ((acc (make-vector (+ MAGIC 1))))
+(define (generate-cache lst n)
+  (let ((acc (make-vector n)))
     (foldl
       (lambda (lst index)
-        (let* ((tmp (make-hash-table)) (lst (map (lambda (_) (apply iterate! tmp _)) lst)))
+        (let* ((tmp (make-hash-table)) (lst (map (lambda (_) (apply iterate-wind! tmp _)) lst)))
           (vector-set! acc index tmp) lst))
-      lst (range 0 MAGIC))
+      lst (range 0 (- n 1)))
     acc))
 
 (define (process-input lst)
-  (let* ((array (list->array lst)) (dims (array-dimensions array)))
+  (let* ((array (list->array lst)) (dimensions (array-dimensions array)))
     (list
       (generate-cache
         (foldl
           (lambda (acc coord)
             (case (array-ref array coord)
-              ((#\^) (cons (list coord '(-1  0) dims) acc))
-              ((#\>) (cons (list coord '( 0  1) dims) acc))
-              ((#\v) (cons (list coord '( 1  0) dims) acc))
-              ((#\<) (cons (list coord '( 0 -1) dims) acc))
+              ((#\^) (cons (list coord '(-1  0) dimensions) acc))
+              ((#\>) (cons (list coord '( 0  1) dimensions) acc))
+              ((#\v) (cons (list coord '( 1  0) dimensions) acc))
+              ((#\<) (cons (list coord '( 0 -1) dimensions) acc))
               (else acc)))
-          '() (array-indexes array)))
-      array '(-1 0) (apply (lambda (h w) (list h (- w 1))) dims))))
+          '() (array-indexes array))
+        (apply lcm dimensions))
+      array '(-1 0) (map + '(-1 0) dimensions))))
 
 (define (import-input)
   (let ((_ (map string->list (read-lines))))
@@ -61,18 +59,18 @@
     (filter
       (lambda (coord)
         (or (array-exists? array coord)
-          (equal? coord from)
-          (equal? coord to)))
+            (equal? coord from)
+            (equal? coord to)))
       (map
         (lambda (offset)
           (map + coord offset))
-        OFFSETS))))
+        offsets))))
 
 (define (find-path vec array from to start)
   (let loop ((i start) (acc (list from)))
     (if (member to acc) i
       (loop (+ i 1)
-        (let ((cache (make-hash-table)) (table (vector-ref vec i)))
+        (let ((cache (make-hash-table)) (table (vector-ref vec (modulo i (vector-length vec)))))
           (foldl
             (lambda (acc coord)
               (foldl
@@ -89,10 +87,10 @@
   (- (find-path vec array from to 0) 1))
 
 (define (solve/2 vec array from to)
-  (let loop ((i 0) (start 0) (from from) (to to))
+  (let loop ((i 0) (acc 0) (from from) (to to))
     (if (= i 3)
-      (- start 1)
-      (loop (+ i 1) (find-path vec array from to start) to from))))
+      (- acc 1)
+      (loop (+ i 1) (find-path vec array from to acc) to from))))
 
 (let ((input (import-input)))
   (print (apply solve/1 input))
