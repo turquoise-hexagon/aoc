52e8f1bdb0a406d087cc2e52998364b584bd70f4
turquoise-hexagon <camille@lzr.pw>
2023-12-01 19:30:08 +0000

    update 2023/01

--- 2023/01/solution.scm
+++ 2023/01/solution.scm
@@ -9,15 +9,15 @@
 
 (define table
   (alist->hash-table
-    '(("one" .   "1")
-      ("two" .   "2")
+    '(("one"   . "1")
+      ("two"   . "2")
       ("three" . "3")
-      ("four" .  "4")
-      ("five" .  "5")
-      ("six" .   "6")
+      ("four"  . "4")
+      ("five"  . "5")
+      ("six"   . "6")
       ("seven" . "7")
       ("eight" . "8")
-      ("nine" .  "9"))))
+      ("nine"  . "9"))))
 
 (define (import-input)
   (read-lines))
