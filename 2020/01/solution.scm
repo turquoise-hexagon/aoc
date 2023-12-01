00568114257ad0a00154e777d1a350ed6fe0450e
turquoise-hexagon <camille@lzr.pw>
2023-12-01 21:47:49 +0000

    update 2020/01

--- 2020/01/solution.scm
+++ 2020/01/solution.scm
@@ -13,6 +13,8 @@
         (= (apply + lst) 2020))
       (combinations input n))))
 
-(let ((input (import-input)))
-  (print (solve input 2))
-  (print (solve input 3)))
+(let* ((input (import-input))
+       (part/1 (solve input 2))
+       (part/2 (solve input 3)))
+  (print part/1) (assert (= part/1 224436))
+  (print part/2) (assert (= part/2 303394260)))
