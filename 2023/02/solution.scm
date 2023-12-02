beb244d49bec46e6bac2a11b14dc1526f128f95d
turquoise-hexagon <camille@lzr.pw>
2023-12-02 05:47:01 +0000

    update 2023/02

--- 2023/02/solution.scm
+++ 2023/02/solution.scm
@@ -1,7 +1,6 @@
 (import
   (chicken io)
   (chicken string)
-  (srfi 1)
   (srfi 69))
 
 (define (parse-id str)
@@ -16,7 +15,7 @@
       (lambda (str)
         (apply
           (lambda (value color)
-            (hash-table-update!/default acc color (lambda (_) (cons (string->number value) _)) '()))
+            (hash-table-update!/default acc color (lambda (_) (max (string->number value) _)) 0))
           (string-split str " ")))
       lst)
     acc))
@@ -30,20 +29,14 @@
 (define (import-input)
   (map parse (read-lines)))
 
-(define (test? data color maximum)
-  (every
-    (lambda (value)
-      (<= value maximum))
-    (hash-table-ref data color)))
-
 (define (solve/1 input)
   (foldl
     (lambda (acc game)
       (apply
         (lambda (id data)
-          (if (and (test? data   "red" 12)
-                   (test? data "green" 13)
-                   (test? data  "blue" 14))
+          (if (and (<= (hash-table-ref data   "red") 12)
+                   (<= (hash-table-ref data "green") 13)
+                   (<= (hash-table-ref data  "blue") 14))
             (+ acc id)
             acc))
         game))
@@ -54,15 +47,15 @@
     (lambda (acc game)
       (apply
         (lambda (_ data)
-          (+ (* (apply max (hash-table-ref data   "red"))
-                (apply max (hash-table-ref data "green"))
-                (apply max (hash-table-ref data  "blue")))
+          (+ (* (hash-table-ref data   "red")
+                (hash-table-ref data "green")
+                (hash-table-ref data  "blue"))
              acc))
         game))
     0 input))
 
-(let* ((input (import-input))
-       (part/1 (solve/1 input))
-       (part/2 (solve/2 input)))
+(let* ((input (time (import-input)))
+       (part/1 (time (solve/1 input)))
+       (part/2 (time (solve/2 input))))
   (print part/1) (assert (= part/1 2006))
   (print part/2) (assert (= part/2 84911)))
