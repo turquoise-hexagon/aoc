84d03536663925f8baabb41a4114dfb79d049fed
turquoise-hexagon <camille@lzr.pw>
2023-12-03 00:45:27 +0000

    update 2021/01

--- 2021/01/solution.scm
+++ 2021/01/solution.scm
@@ -8,8 +8,8 @@
 (define (solve input n)
   (count < input (drop input n)))
 
-(let* ((input (import-input))
-       (part/1 (solve input 1))
-       (part/2 (solve input 3)))
-  (print part/1) (assert (= part/1 1387))
-  (print part/2) (assert (= part/2 1362)))
+(let ((input (import-input)))
+  (let ((part/1 (solve input 1)))
+    (print part/1) (assert (= part/1 1387)))
+  (let ((part/2 (solve input 3)))
+    (print part/2) (assert (= part/2 1362))))
