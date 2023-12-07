da4324401fc3191553780f9b092bb502ac11d295
turquoise-hexagon <camille@lzr.pw>
2023-12-07 11:19:07 +0000

    update 2022/07

--- 2023/07/solution.scm
+++ 2023/07/solution.scm
@@ -68,12 +68,12 @@
 (define (solve input proc)
   (apply +
     (map
-      (lambda (lst index)
-        (bind (_ _ bid) lst (* index bid)))
+      (lambda (i index)
+        (bind (_ _ bid) i (* index bid)))
       (sort
         (map
-          (lambda (lst)
-            (bind (hand bid) lst (let ((_ (map card-value hand))) (list _ (proc _) bid))))
+          (lambda (i)
+            (bind (hand bid) i (let ((_ (map card-value hand))) (list _ (proc _) bid))))
           input)
         (lambda (a b)
           (bind (hand/a value/a _ hand/b value/b _) (append a b)
