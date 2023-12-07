709ce8c8005dc52dc06ec58009ca08d723fb9638
turquoise-hexagon <camille@lzr.pw>
2023-12-07 19:16:44 +0000

    update 2022/07

--- 2023/07/solution.scm
+++ 2023/07/solution.scm
@@ -17,18 +17,8 @@
     (for-each
       (lambda (card value)
         (hash-table-set! acc card value))
-      (string->list cards) (iota (string-length cards) 2))
+      (string->list cards) (iota (string-length cards) 1))
     acc))
-
-(define hand-values
-  (alist->hash-table
-    '(((1 1 1 1 1) . 1)
-      ((2 1 1 1)   . 2)
-      ((2 2 1)     . 3)
-      ((3 1 1)     . 4)
-      ((3 2)       . 5)
-      ((4 1)       . 6)
-      ((5)         . 7))))
 
 (define (import-input)
   (map
@@ -46,15 +36,15 @@
       (lambda (i)
         (hash-table-update!/default acc i add1 0))
       lst)
-    (hash-table-ref hand-values (sort (hash-table-values acc) >))))
+    (- (apply max (hash-table-values acc)) (hash-table-size acc))))
 
 (define (hand-value/2 lst)
-  (if (member 1 lst)
+  (if (member 0 lst)
     (apply max
       (map hand-value/1
         (map
           (lambda (value)
-            (map (lambda (i) (if (= i 1) value i)) lst))
+            (map (lambda (i) (if (= i 0) value i)) lst))
           (hash-table-values card-values))))
     (hand-value/1 lst)))
 
@@ -86,6 +76,6 @@
 (let ((input (import-input)))
   (let ((part/1 (solve input hand-value/1)))
     (print part/1) (assert (= part/1 253933213)))
-  (hash-table-set! card-values #\J 1)
+  (hash-table-set! card-values #\J 0)
   (let ((part/2 (solve input hand-value/2)))
     (print part/2) (assert (= part/2 253473930))))
