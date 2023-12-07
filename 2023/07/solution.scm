1253c545babdf07309e45165b54081722d4648fe
turquoise-hexagon <camille@lzr.pw>
2023-12-07 07:08:52 +0000

    add 2023/07

---
+++ 2023/07/solution.scm
@@ -0,0 +1,111 @@
+(import
+  (chicken io)
+  (chicken sort)
+  (chicken string)
+  (srfi 1)
+  (srfi 69))
+
+(define-syntax bind
+  (syntax-rules ()
+    ((_ pattern data expression expression* ...)
+     (apply (lambda pattern expression expression* ...) data))))
+
+(define card-values
+  (alist->hash-table
+    '((#\2 .  2)
+      (#\3 .  3)
+      (#\4 .  4)
+      (#\5 .  5)
+      (#\6 .  6)
+      (#\7 .  7)
+      (#\8 .  8)
+      (#\9 .  9)
+      (#\T . 10)
+      (#\J . 11)
+      (#\Q . 12)
+      (#\K . 13)
+      (#\A . 14))))
+
+(define hand-values
+  (alist->hash-table
+    '(((5)         . 7)
+      ((4 1)       . 6)
+      ((3 2)       . 5)
+      ((3 1 1)     . 4)
+      ((2 2 1)     . 3)
+      ((2 1 1 1)   . 2)
+      ((1 1 1 1 1) . 1))))
+
+(define (parse str)
+  (bind (hand bid) (string-split str " ")
+    (list (string->list hand) (string->number bid))))
+
+(define (import-input)
+  (map parse (read-lines)))
+
+(define (card-value n)
+  (hash-table-ref card-values n))
+
+(define (card-counts lst)
+  (let ((acc (make-hash-table)))
+    (for-each
+      (lambda (i)
+        (hash-table-update!/default acc i add1 0))
+      lst)
+    (hash-table-values acc)))
+
+(define (strongest-first a b)
+  (if (or (null? a)
+          (null? b))
+    #t
+    (if (= (car a)
+           (car b))
+      (strongest-first (cdr a)
+                       (cdr b))
+      (< (car a)
+         (car b)))))
+
+(define (hand-value/1 lst)
+  (hash-table-ref hand-values (sort (card-counts lst) >)))
+
+(define (hand-value/2 lst)
+  (if (member 1 lst)
+    (apply max
+      (map hand-value/1
+        (map
+          (lambda (value)
+            (map
+              (lambda (i)
+                (if (= i 1) value i))
+              lst))
+          (hash-table-values card-values))))
+    (hand-value/1 lst)))
+
+(define-inline (compare? a b)
+  (bind (a _ b _) (append a b)
+    (let ((m (proc a))
+          (n (proc b)))
+      (cond ((< m n) #t)
+            ((= m n)
+             (strongest-first a b))
+            (else #f)))))
+
+(define (solve input proc)
+  (apply +
+    (map
+      (lambda (lst index)
+        (bind (_ bid) lst (* index bid)))
+      (sort
+        (map
+          (lambda (lst)
+            (bind (hand bid) lst (list (map card-value hand) bid)))
+          input)
+        compare?)
+      (iota (length input) 1))))
+
+(let ((input (import-input)))
+  (let ((part/1 (solve input hand-value/1)))
+    (print part/1) (assert (= part/1 253933213)))
+  (hash-table-set! card-values #\J 1)
+  (let ((part/2 (solve input hand-value/2)))
+    (print part/2) (assert (= part/2 253473930))))
