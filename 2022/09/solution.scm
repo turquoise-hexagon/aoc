e3855fa78cfd13e1fd3762f646bcdf24b9b13483
turquoise-hexagon <camille@lzr.pw>
2023-12-09 12:09:44 +0000

    update 2022/09

--- 2022/09/solution.scm
+++ 2022/09/solution.scm
@@ -50,5 +50,7 @@
     (hash-table-size acc)))
 
 (let ((input (import-input)))
-  (print (solve input 2))
-  (print (solve input 10)))
+  (let ((part/1 (solve input 2)))
+    (print part/1) (assert (= part/1 6044)))
+  (let ((part/2 (solve input 10)))
+    (print part/2) (assert (= part/2 2384))))
