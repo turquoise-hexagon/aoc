1fbfffeb9eea65281fae505b7b94d8108efdb5ef
turquoise-hexagon <camille@lzr.pw>
2023-12-11 07:03:17 +0000

    update 2023/11

--- 2023/11/solution.scm
+++ 2023/11/solution.scm
@@ -3,58 +3,70 @@
   (euler)
   (srfi 1))
 
-(define (process array a b  proc)
+(define (pairs array)
+  (combinations
+    (filter
+      (lambda (i)
+        (char=? (array-ref array i) #\#))
+      (array-indexes array))
+    2))
+
+(define (_empty array a b fun)
   (filter
     (lambda (a)
       (every
         (lambda (b)
-          (char=? (array-ref array (proc a b)) #\.))
+          (char=? (array-ref array (fun a b)) #\.))
         b))
     a))
 
 (define (empty array)
   (apply
     (lambda (h w)
-      (let ((h (iota h))
-            (w (iota w)))
-        (list
-          (process array h w (lambda (a b) (list a b)))
-          (process array w h (lambda (a b) (list b a))))))
-    (array-dimensions array)))
+      (list
+        (_empty array h w (lambda (a b) (list a b)))
+        (_empty array w h (lambda (a b) (list b a)))))
+    (map iota (array-dimensions array))))
 
-(define (galaxies array)
-  (filter
+(define (manhattan a b)
+  (apply + (map abs (map - a b))))
+
+(define (counts a b empty)
+  (apply +
+    (map
+      (lambda (a b i)
+        (let ((a (min a b))
+              (b (max a b)))
+          (count
+            (lambda (i)
+              (<= a i b))
+            i)))
+      a b empty)))
+
+(define (process pairs empty)
+  (map
     (lambda (i)
-      (char=? (array-ref array i) #\#))
-    (array-indexes array)))
+      (apply
+        (lambda (a b)
+          (cons (manhattan a b) (counts a b empty)))
+        i))
+    pairs))
 
 (define (import-input)
   (let ((array (list->array (map string->list (read-lines)))))
-    (values (combinations (galaxies array) 2) (empty array))))
+    (process
+      (pairs array)
+      (empty array))))
 
-(define (distance empty expansion a b)
-  (+ (apply + (map abs (map - a b)))
-     (* (apply +
-          (map
-            (lambda (a b i)
-              (let ((a (min a b))
-                    (b (max a b)))
-                (count
-                  (lambda (i)
-                    (<= a i b))
-                  i)))
-            a b empty))
-        (- expansion 1))))
-
-(define (solve pairs empty expansion)
+(define (solve input expansion)
   (apply +
     (map
       (lambda (i)
-        (apply distance empty expansion i))
-      pairs)))
+        (+ (car i) (* (cdr i) (- expansion 1))))
+      input)))
 
-(let-values (((pairs empty) (import-input)))
-  (let ((part/1 (solve pairs empty 2)))
+(let ((input (import-input)))
+  (let ((part/1 (solve input 2)))
     (print part/1) (assert (= part/1 10292708)))
-  (let ((part/2 (solve pairs empty 1000000)))
+  (let ((part/2 (solve input 1000000)))
     (print part/2) (assert (= part/2 790194712336))))
