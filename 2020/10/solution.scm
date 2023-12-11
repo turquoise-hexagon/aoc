e17ae050456d069875c96e52d2334c64cbeb4ba5
turquoise-hexagon <camille@lzr.pw>
2023-12-11 09:34:39 +0000

    update 2020/10

--- 2020/10/solution.scm
+++ 2020/10/solution.scm
@@ -3,32 +3,34 @@
   (chicken sort)
   (srfi 1))
 
-(define (process-input lst)
-  (let ((lst (sort (append `(,(+ (apply max lst) 3) 0) lst) >)))
+(define (process lst)
+  (let ((lst (sort (cons* 0 (+ (apply max lst) 3) lst) >)))
     (map - lst (cdr lst))))
 
 (define (import-input)
-  (process-input (map string->number (read-lines))))
+  (process (map string->number (read-lines))))
 
 (define (tribonacci n)
-  (let tribonacci/h ((a 0) (b 0) (c 1) (n n))
+  (let loop ((a 0) (b 0) (c 1) (n n))
     (if (= n 0)
       a
-      (tribonacci/h b c (+ a b c) (- n 1)))))
+      (loop b c (+ a b c) (- n 1)))))
 
 (define (solve/1 input)
-  (* (count (cut = 1 <>) input)
-     (count (cut = 3 <>) input)))
+  (* (count (lambda (i) (= i 1)) input)
+     (count (lambda (i) (= i 3)) input)))
 
 (define (solve/2 input)
   (let ((lst (foldl
-               (lambda (acc n)
-                 (if (= n 1)
-                   (cons (+ 1 (car acc)) (cdr acc))
+               (lambda (acc i)
+                 (if (= i 1)
+                   (cons (+ (car acc) 1) (cdr acc))
                    (cons 0 acc)))
                '(0) input)))
-    (apply * (map tribonacci (map (cut + <> 2) lst)))))
+    (apply * (map tribonacci (map (lambda (i) (+ i 2)) lst)))))
 
 (let ((input (import-input)))
-  (print (solve/1 input))
-  (print (solve/2 input)))
+  (let ((part/1 (solve/1 input)))
+    (print part/1) (assert (= part/1 2450)))
+  (let ((part/2 (solve/2 input)))
+    (print part/2) (assert (= part/2 32396521357312))))
