2b3162291f55f0b40a026e1ec27abdbf19d32522
turquoise-hexagon <camille@lzr.pw>
2023-12-11 06:35:31 +0000

    add 2023/11

---
+++ 2023/11/solution.scm
@@ -0,0 +1,60 @@
+(import
+  (chicken io)
+  (euler)
+  (srfi 1))
+
+(define (process array a b  proc)
+  (filter
+    (lambda (a)
+      (every
+        (lambda (b)
+          (char=? (array-ref array (proc a b)) #\.))
+        b))
+    a))
+
+(define (empty array)
+  (apply
+    (lambda (h w)
+      (let ((h (iota h))
+            (w (iota w)))
+        (list
+          (process array h w (lambda (a b) (list a b)))
+          (process array w h (lambda (a b) (list b a))))))
+    (array-dimensions array)))
+
+(define (import-input)
+  (let ((array (list->array (map string->list (read-lines)))))
+    (values array (empty array))))
+
+(define (galaxies array)
+  (filter
+    (lambda (i)
+      (char=? (array-ref array i) #\#))
+    (array-indexes array)))
+
+(define (distance empty expansion a b)
+  (+ (apply + (map abs (map - a b)))
+     (* (apply +
+          (map
+            (lambda (a b i)
+              (let ((a (min a b))
+                    (b (max a b)))
+                (count
+                  (lambda (i)
+                    (<= a i b))
+                  i)))
+            a b empty))
+        (- expansion 1))))
+
+(define (solve array empty expansion)
+  (apply +
+    (map
+      (lambda (i)
+        (apply distance empty expansion i))
+      (combinations (galaxies array) 2))))
+
+(let-values (((array empty) (import-input)))
+  (let ((part/1 (solve array empty 2)))
+    (print part/1) (assert (= part/1 10292708)))
+  (let ((part/2 (solve array empty 1000000)))
+    (print part/2) (assert (= part/2 790194712336))))
