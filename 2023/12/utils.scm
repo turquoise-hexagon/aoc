502160205b7883b0175ceb4cf3883bbe23d95191
turquoise-hexagon <camille@lzr.pw>
2023-12-12 08:34:27 +0000

    update 2023/12

--- 2023/12/utils.scm
+++ 2023/12/utils.scm
@@ -2,16 +2,16 @@
   (cond
     ((null? l) l)
     ((zero? n) l)
-    (else (_drop (cdr l) (- n 1)))))
+    (else (_drop (##sys#slot l 1) (- n 1)))))
 
 (define (_take l n)
   (cond
     ((null? l) '())
     ((zero? n) '())
-    (else (cons (##sys#slot l 0) (_take (cdr l) (- n 1))))))
+    (else (cons (##sys#slot l 0) (_take (##sys#slot l 1) (- n 1))))))
 
 (define (_list-ref l n)
   (cond
     ((null? l) #f)
     ((zero? n) (##sys#slot l 0))
-    (else (_list-ref (cdr l) (- n 1)))))
+    (else (_list-ref (##sys#slot l 1) (- n 1)))))
