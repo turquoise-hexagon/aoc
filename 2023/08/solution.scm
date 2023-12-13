86dbd3219a8984ffc3aa3dad5cadf10c131fb9a0
turquoise-hexagon <camille@lzr.pw>
2023-12-13 13:47:30 +0000

    update 2023/08

--- 2023/08/solution.scm
+++ 2023/08/solution.scm
@@ -1,10 +1,11 @@
 (import
   (chicken io)
-  (chicken irregex)
+  (chicken string)
+  (srfi 1)
   (srfi 69))
 
 (define (parse-instructions str)
-  (list->vector
+  (apply circular-list
     (map
       (lambda (i)
         (case i
@@ -12,40 +13,41 @@
           ((#\R) cdr)))
       (string->list str))))
 
-(define (parse-network str)
+(define (parse-network lst)
   (let ((acc (make-hash-table)))
     (for-each
       (lambda (i)
         (apply
           (lambda (current left right)
             (hash-table-set! acc current (cons left right)))
-          (irregex-split "[ =(,)]" i)))
-      (irregex-split "\n" str))
+          (string-split i " =(,)")))
+      lst)
     acc))
 
 (define (import-input)
-  (apply
-    (lambda (instructions network)
-      (values (parse-instructions instructions) (parse-network network)))
-    (irregex-split "\n\n" (read-string))))
+   (apply
+     (lambda (instructions _ . network)
+       (values (parse-instructions instructions) (parse-network network)))
+     (read-lines)))
 
-(define (run instructions network from)
-  (let ((len (vector-length instructions)))
-    (let loop ((i 0) (node from))
-      (if (char=? (string-ref node 2) #\Z)
-        i
-        (loop (+ i 1) ((vector-ref instructions (modulo i len)) (hash-table-ref network node)))))))
+(define (run instructions network node)
+  (let loop ((instructions instructions) (node node) (acc 0))
+    (if (char=? (string-ref node 2) #\Z)
+      acc
+      (loop (cdr instructions) ((car instructions) (hash-table-ref network node)) (+ acc 1)))))
 
 (define (solve/1 instructions network)
   (run instructions network "AAA"))
 
 (define (solve/2 instructions network)
-  (foldl
-    (lambda (acc i)
-      (if (char=? (string-ref i 2) #\A)
-        (lcm acc (run instructions network i))
-        acc))
-    1 (hash-table-keys network)))
+  (apply lcm
+    (map
+      (lambda (i)
+        (run instructions network i))
+      (filter
+        (lambda (i)
+          (char=? (string-ref i 2) #\A))
+        (hash-table-keys network)))))
 
 (let-values (((instructions network) (import-input)))
   (let ((part/1 (solve/1 instructions network)))
