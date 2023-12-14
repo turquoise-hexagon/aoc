9203d2bb8cb55a26e86daf30d7afc7a3c0d5f71e
turquoise-hexagon <camille@lzr.pw>
2023-12-14 06:52:20 +0000

    update 2023/14

--- 2023/14/solution.scm
+++ 2023/14/solution.scm
@@ -14,25 +14,26 @@
         (loop next)
         i))))
 
-(define (iterate! array direction)
-  (foldl
-    (lambda (acc coord)
-      (if (char=? (array-ref array coord) #\O)
-        (let ((next (search array coord direction)))
-          (if (equal? coord next)
-            acc
-            (begin
-              (array-set! array coord #\.)
-              (array-set! array next  #\O)
-              #t)))
-        acc))
-    #f (array-indexes array)))
+(define (indexes array direction)
+  (apply product
+    (map
+      (lambda (i lst)
+        (case i
+          ((-1) lst)
+          (( 0) lst)
+          (( 1) (reverse lst))))
+      direction (map iota (array-dimensions array)))))
 
 (define (tilt array direction)
-  (let ((acc (array-copy array)))
-    (let loop ()
-      (when (iterate! acc direction)
-        (loop)))
+  (let ((acc array))
+    (for-each
+      (lambda (coord)
+        (when (char=? (array-ref acc coord) #\O)
+          (let ((next (search acc coord direction)))
+            (unless (equal? coord next)
+              (array-set! acc coord #\.)
+              (array-set! acc next  #\O)))))
+      (indexes acc direction))
     acc))
 
 (define (score array)
@@ -50,21 +51,28 @@
   (apply string-append (map list->string (array->list array))))
 
 (define (solve/1 input)
-  (score (tilt input '(-1 0))))
+  (let ((acc (array-copy input)))
+    (tilt acc '(-1 0)) (score acc)))
 
 (define (solve/2 input n)
   (let ((cache (make-hash-table)))
-    (let loop ((i 1) (acc input))
+    (let loop ((i 1) (acc (array-copy input)))
       (if (= i n)
         (score acc)
-        (let* ((acc (foldl tilt acc '((-1 0) (0 -1) (1 0) (0 1)))) (id (id acc)))
-          (if (hash-table-exists? cache id)
-            (let ((_ (hash-table-ref cache id)))
-              (hash-table-clear! cache)
-              (loop (- n (modulo (- n _) (- i _))) acc))
-            (begin
-              (hash-table-set! cache id i)
-              (loop (+ i 1) acc))))))))
+        (begin
+          (for-each (lambda (i) (tilt acc i)) '((-1 0) (0 -1) (1 0) (0 1)))
+          (let ((id (id acc)))
+            (if (hash-table-exists? cache id)
+              (let ((_ (hash-table-ref cache id)))
+                (hash-table-clear! cache)
+                (loop (- n (modulo (- n _) (- i _))) acc))
+              (begin
+                (hash-table-set! cache id i)
+                (loop (+ i 1) acc)))))))))
+
+(define (output array)
+  (for-each print (map list->string (array->list array)))
+  (print))
 
 (let ((input (import-input)))
   (let ((part/1 (solve/1 input)))
