49fdf749bad91f5e979fa256cf41d890612a3f89
turquoise-hexagon <camille@lzr.pw>
2023-12-24 22:40:00 +0100

    update 2023/24

--- 2023/24/solution.scm
+++ 2023/24/solution.scm
@@ -2,7 +2,6 @@
   (chicken io)
   (chicken string)
   (euler)
-  (matchable)
   (srfi 1))
 
 (define (import-input)
@@ -13,23 +12,25 @@
 
 (define (solve input)
   (count
-    (match-lambda
-      (((xa1 ya1 _ dxa dya _)
-        (xb1 yb1 _ dxb dyb _))
-       (let* ((sa (/ (- ya1 (+ ya1 dya)) (- xa1 (+ xa1 dxa))))
-              (sb (/ (- yb1 (+ yb1 dyb)) (- xb1 (+ xb1 dxb))))
-              (oa (- ya1 (* sa xa1)))
-              (ob (- yb1 (* sb xb1))))
-         (if (= sa sb) #f
-           (let* ((_ (/ (- oa ob) (- sb sa)))
-                  (xi _) (yi (+ (* sa _) oa)))
-             (if (and (= (signum (- xi xa1)) (signum dxa))
-                      (= (signum (- yi ya1)) (signum dya))
-                      (= (signum (- xi xb1)) (signum dxb))
-                      (= (signum (- yi yb1)) (signum dyb)))
-               (and (< #e2e14 xi #e4e14)
-                    (< #e2e14 yi #e4e14))
-               #f))))))
+    (lambda (i)
+      (apply
+        (lambda (xa1 ya1 _ dxa dya _
+                 xb1 yb1 _ dxb dyb _)
+          (let* ((sa (/ (- ya1 (+ ya1 dya)) (- xa1 (+ xa1 dxa))))
+                 (sb (/ (- yb1 (+ yb1 dyb)) (- xb1 (+ xb1 dxb))))
+                 (oa (- ya1 (* sa xa1)))
+                 (ob (- yb1 (* sb xb1))))
+            (if (= sa sb) #f
+              (let* ((_ (/ (- oa ob) (- sb sa)))
+                     (xi _) (yi (+ (* sa _) oa)))
+                (if (and (= (signum (- xi xa1)) (signum dxa))
+                         (= (signum (- yi ya1)) (signum dya))
+                         (= (signum (- xi xb1)) (signum dxb))
+                         (= (signum (- yi yb1)) (signum dyb)))
+                  (and (< #e2e14 xi #e4e14)
+                       (< #e2e14 yi #e4e14))
+                  #f)))))
+        (join i)))
     (combinations input 2)))
 
 (let ((part (solve (import-input))))
