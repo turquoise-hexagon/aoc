5f0e500f8b6d4a40f3c6b25c3bcd83233c0f10c1
turquoise-hexagon <camille@lzr.pw>
2023-12-28 10:19:59 +0100

    add 2015/9

---
+++ 2015/09/solution.scm
@@ -0,0 +1,44 @@
+(import
+  (chicken io)
+  (chicken string)
+  (euler)
+  (srfi 69))
+
+(define (connect! graph a b dist)
+  (hash-table-update!/default graph a
+    (lambda (value)
+      (hash-table-set! value b dist)
+      value)
+    (make-hash-table)))
+
+(define (process graph)
+  (map
+    (lambda (i)
+      (apply +
+        (map
+          (lambda (a b)
+            (hash-table-ref (hash-table-ref graph a) b))
+          i (cdr i))))
+    (permutations (hash-table-keys graph))))
+
+(define (import-input)
+  (let ((acc (make-hash-table)))
+    (for-each
+      (lambda (i)
+        (apply
+          (lambda (a _ b _ dist)
+            (let ((dist (string->number dist)))
+              (connect! acc a b dist)
+              (connect! acc b a dist)))
+          (string-split i " ")))
+      (read-lines))
+    (process acc)))
+
+(define (solve input proc)
+  (foldl proc (car input) (cdr input)))
+
+(let ((input (import-input)))
+  (let ((part/1 (solve input min)))
+    (print part/1))
+  (let ((part/2 (solve input max)))
+    (print part/2)))
