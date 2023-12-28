cafc038f8fbcd8f9039650cc469c7d3f5f0f1e67
turquoise-hexagon <camille@lzr.pw>
2023-12-28 21:55:46 +0100

    add 2015/17

---
+++ 2015/17/solution.scm
@@ -0,0 +1,31 @@
+(import
+  (chicken io)
+  (euler)
+  (srfi 1))
+
+(define (import-input)
+  (let ((lst (map string->number (read-lines))))
+    (filter
+      (lambda (i)
+        (= (apply + i) 150))
+      (join
+        (map
+          (lambda (i)
+            (combinations lst i))
+          (range 1 (length lst)))))))
+
+(define (solve/1 input)
+  (length input))
+
+(define (solve/2 input)
+  (let ((minimum (apply min (map length input))))
+    (count
+      (lambda (i)
+        (= (length i) minimum))
+      input)))
+
+(let ((input (import-input)))
+  (let ((part/1 (solve/1 input)))
+    (print part/1))
+  (let ((part/2 (solve/2 input)))
+    (print part/2)))
