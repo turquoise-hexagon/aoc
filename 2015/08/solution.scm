7ba97c42d5fe7c24174aec0c158f956916a88cc4
turquoise-hexagon <camille@lzr.pw>
2023-12-30 05:15:38 +0000

    add 2015/08

---
+++ 2015/08/solution.scm
@@ -0,0 +1,34 @@
+(import
+  (chicken io)
+  (chicken irregex))
+
+(define (import-input)
+  (read-lines))
+
+(define (proc/1 str)
+  (let*
+    ((_ str)
+     (_ (irregex-replace/all "(^\"|\"$)"           _  ""))
+     (_ (irregex-replace/all "\\\\\\\\"            _ "#"))
+     (_ (irregex-replace/all "\\\\\""              _ "#"))
+     (_ (irregex-replace/all "\\\\x[a-fA-F0-9]{2}" _ "#")))
+    (- (string-length str)
+       (string-length _))))
+
+(define (proc/2 str)
+  (let*
+    ((_ str)
+     (_ (irregex-replace/all "\\\\" _ "\\\\"))
+     (_ (irregex-replace/all "\""   _ "\\\"")))
+    (- (string-length _)
+       (string-length str)
+       -2)))
+
+(define (solve input proc)
+  (apply + (map proc input)))
+
+(let ((input (import-input)))
+  (let ((part/1 (solve input proc/1)))
+    (print part/1) (assert (= part/1 1342)))
+  (let ((part/2 (solve input proc/2)))
+    (print part/2) (assert (= part/2 2074))))
