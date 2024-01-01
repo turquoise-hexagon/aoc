6412a3f77793f5ec68e8fffd56db702b16f8b952
turquoise-hexagon <camille@lzr.pw>
2024-01-01 16:12:31 +0000

    add 2016/08

---
+++ 2016/08/solution.scm
@@ -0,0 +1,46 @@
+(import
+  (chicken io)
+  (chicken irregex)
+  (euler)
+  (srfi 1))
+
+(define-constant h 6)
+(define-constant w 50)
+
+(define (process input)
+  (foldl
+    (lambda (acc i)
+      (let ((next (array-copy acc)))
+        (apply
+          (lambda (t a b)
+            (case t
+              ((rect)   (for-each (lambda (i) (array-set! next i #\#)) (product (iota b) (iota a))))
+              ((row)    (for-each (lambda (i) (array-set! next i (array-ref acc (map modulo (map - i (list 0 b)) (list h w))))) (product (list a) (iota w))))
+              ((column) (for-each (lambda (i) (array-set! next i (array-ref acc (map modulo (map - i (list b 0)) (list h w))))) (product (iota h) (list a))))))
+          i)
+        next))
+    (make-array (list h w) #\ ) input))
+
+(define (import-input)
+  (process
+    (map
+      (lambda (i)
+        (apply
+          (lambda (t a b)
+            (list
+              (string->symbol t)
+              (string->number a)
+              (string->number b)))
+          (irregex-split "( |rotate|[xy]=|by|x)" i)))
+      (read-lines))))
+
+(define (solve input)
+  (count
+    (lambda (i)
+      (char=? (array-ref input i) #\#))
+    (array-indexes input)))
+
+(let ((input (import-input)))
+  (let ((part (solve input)))
+    (print part) (assert (= part 110)))
+  (for-each print (map list->string (array->list input))))
