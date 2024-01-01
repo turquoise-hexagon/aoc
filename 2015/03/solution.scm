2efa6da6df01648224f0c050ee547fb360feb18f
turquoise-hexagon <camille@lzr.pw>
2024-01-01 08:37:17 +0000

    update 2015/03

--- 2015/03/solution.scm
+++ 2015/03/solution.scm
@@ -1,27 +1,15 @@
 (import
   (chicken io)
-  (srfi 1))
-
-(define-constant MAGIC #e1e5)
+  (srfi 1)
+  (srfi 69))
 
 (define (import-input)
   (string->list (read-line)))
 
-(define (adjust n)
-  (if (< n 0)
-    (- (+ n n 1))
-    (+ n n)))
-
-(define (cantor a b)
-  (let* ((a (adjust a))
-         (b (adjust b))
-         (_ (+ a b)))
-    (+ (quotient (* _ (+ _ 1)) 2) b)))
-
-(define (run! vec lst)
+(define (run! table lst)
   (foldl
     (lambda (coord i)
-      (vector-set! vec (apply cantor coord) #t)
+      (hash-table-set! table coord #t)
       (map + coord
         (case i
           ((#\^) '(-1  0))
@@ -30,24 +18,17 @@
           ((#\<) '( 0 -1)))))
     '(0 0) lst))
 
-(define (value vec)
-  (do ((i 0 (+ i 1))
-       (acc 0 (if (vector-ref vec i)
-                (+ acc 1)
-                acc)))
-    ((= i MAGIC) acc)))
-
 (define (solve/1 input)
-  (let ((acc (make-vector MAGIC #f)))
+  (let ((acc (make-hash-table)))
     (run! acc input)
-    (value acc)))
+    (hash-table-size acc)))
 
 (define (solve/2 input)
-  (let ((acc (make-vector MAGIC #f)))
+  (let ((acc (make-hash-table)))
     (let ((_ (iota (length input))))
       (run! acc (compress (map even? _) input))
       (run! acc (compress (map odd?  _) input)))
-    (value acc)))
+    (hash-table-size acc)))
 
 (let ((input (import-input)))
   (let ((part/1 (solve/1 input)))
