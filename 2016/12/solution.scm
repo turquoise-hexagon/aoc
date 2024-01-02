a42ca4d090e89a0cdb30724ed26bdbba43ee9dbe
turquoise-hexagon <camille@lzr.pw>
2024-01-02 12:55:05 +0000

    add 2016/23

--- 2016/12/solution.scm
+++ 2016/12/solution.scm
@@ -1,49 +1,57 @@
 (import
+  (chicken fixnum)
   (chicken io)
-  (chicken string)
-  (srfi 69))
+  (chicken string))
+
+(define (address n)
+  (case (string->symbol n)
+    ((a) '(0))
+    ((b) '(1))
+    ((c) '(2))
+    ((d) '(3))))
+
+(define (convert n)
+  (let ((r (string->number n)))
+    (if r r (address n))))
 
 (define (import-input)
-  (list->vector
-    (map
-      (lambda (i)
-        (string-split i " "))
-      (read-lines))))
+  (map
+    (lambda (i)
+      (apply
+        (lambda (op . l)
+          (cons (string->symbol op) (map convert l)))
+        (string-split i " ")))
+    (read-lines)))
 
-(define-inline (convert n)
-  (let ((_ (string->number n)))
-    (if _ _ (hash-table-ref/default acc n 0))))
+(define-inline (get n)
+  (if (number? n) n (vector-ref acc (car n))))
+
+(define-inline (set n v)
+  (vector-set! acc (car n) v))
 
 (define (solve input init)
-  (let ((acc (make-hash-table)) (len (vector-length input)))
-    (hash-table-set! acc "c" init)
+  (let ((acc (make-vector 4 0)) (limit (vector-length input)))
+    (set (address "c") init)
     (let loop ((i 0))
-      (if (>= i len)
-        (hash-table-ref acc "a")
+      (if (fx>= i limit)
+        (get (address "a"))
         (apply
           (case-lambda
             ((op a)
-             (hash-table-update!/default acc a
-               (case (string->symbol op)
-                 ((inc) add1)
-                 ((dec) sub1))
-               0)
-             (loop (+ i 1)))
+             (case op
+               ((inc) (set a (fx+ (get a) 1)) (loop (fx+ i 1)))
+               ((dec) (set a (fx- (get a) 1)) (loop (fx+ i 1)))))
             ((op a b)
-             (case (string->symbol op)
+             (case op
                ((cpy)
-                (hash-table-set! acc b (convert a))
-                (loop (+ i 1)))
+                (set b (get a))
+                (loop (fx+ i 1)))
                ((jnz)
-                (let ((a (convert a))
-                      (b (convert b)))
-                  (if (= a 0)
-                    (loop (+ i 1))
-                    (loop (+ i b))))))))
+                (loop (fx+ i (if (fx= (get a) 0) 1 (get b))))))))
           (vector-ref input i))))))
 
 (let ((input (import-input)))
-  (let ((part/1 (solve input 0)))
+  (let ((part/1 (solve (list->vector input) 0)))
     (print part/1) (assert (= part/1 318077)))
-  (let ((part/2 (solve input 1)))
+  (let ((part/2 (solve (list->vector input) 1)))
     (print part/2) (assert (= part/2 9227731))))
