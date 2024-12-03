bff2c868e623fb9cb94444a1e7dcbff6ccf8e5ea
turquoise-hexagon <camille@lzr.pw>
2024-12-03 09:30:09 +0000

    add 2024/03

---
+++ 2024/03/solution.scm
@@ -0,0 +1,20 @@
+(import
+  (chicken io)
+  (chicken irregex))
+
+(define (solve input part)
+  (set! flag #t)
+  (foldl
+    (lambda (acc i)
+      (cond
+        ((string=? i    "do()") (set! flag #t) acc)
+        ((string=? i "don't()") (set! flag #f) acc)
+        ((or part flag) (+ acc (apply * (map string->number (irregex-extract "[0-9]+" i)))))
+        (else acc)))
+    0 (irregex-extract "(do\\(\\)|don\'t\\(\\)|mul\\([0-9]+,[0-9]+\\))" input)))
+
+(let ((input (read-string)))
+  (let ((part/1 (solve input #t)))
+    (print part/1) (assert (= part/1 170778545)))
+  (let ((part/2 (solve input #f)))
+    (print part/2) (assert (= part/2 82868252))))
