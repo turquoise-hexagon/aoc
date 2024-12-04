1d53a16022696e5f0fa971e666221ba2cc76fa9c
turquoise-hexagon <camille@lzr.pw>
2024-12-04 12:24:29 +0000

    add 2024/04

--- 2024/04/solution.scm
+++ 2024/04/solution.scm
@@ -1,45 +1,46 @@
 (import
   (chicken io)
   (euler)
-  (euler-syntax)
   (srfi 1))
+
+(define offsets/1 (delete-first (power '(-1 0 1) 2) '(0 0) equal?))
+(define offsets/2 (delete-first (power '(-1   1) 2) '(0 0) equal?))
 
 (define (import-input)
   (list->array (map string->list (read-lines))))
 
+(define (_search array pattern coord offset)
+  (let loop ((coord coord) (pattern pattern) (acc '()))
+    (if (null? pattern)
+      acc
+      (if (and (array-exists? array coord) (char=? (array-ref array coord) (car pattern)))
+        (loop (map + coord offset) (cdr pattern) (cons coord acc))
+        #f))))
+
 (define (search array pattern offsets)
-  (filter
-    (lambda (coords)
-      (equal?
-        (map
-          (lambda (coord)
-            (if (array-exists? array coord)
-              (array-ref array coord)
-              ""))
-          coords)
-        pattern))
+  (let ((pattern (string->list pattern)))
     (append-map
       (lambda (offset)
-        (map
+        (filter-map
           (lambda (coord)
-            (let loop ((coord coord) (pattern pattern))
-              (if (null? pattern)
-                '()
-                (cons coord (loop (map + coord offset) (cdr pattern))))))
+            (_search array pattern coord offset))
           (array-indexes array)))
       offsets)))
 
 (define (solve/1 input)
- (length (search input '(#\X #\M #\A #\S) '((-1 -1) (-1 0) (-1 1) (0 -1) (0 1) (1 -1) (1 0) (1 1)))))
+  (length (search input "XMAS" offsets/1)))
 
 (define (solve/2 input)
-  (count
-    (lambda (lst)
-      (bind (a b) lst
-        (equal?
-          (cadr a)
-          (cadr b))))
-    (combinations (search input '(#\M #\A #\S) '((-1 -1) (-1 1) (1 -1) (1 1))) 2)))
+  (let loop/1 ((lst/1 (search input "MAS" offsets/2)) (acc/1 0))
+    (if (null? lst/1)
+      acc/1
+      (let loop/2 ((lst/2 (cdr lst/1)) (acc/2 0))
+        (if (null? lst/2)
+          (loop/1 (cdr lst/1) (+ acc/1 acc/2))
+          (loop/2 (cdr lst/2)
+            (if (equal? (cadar lst/1) (cadar lst/2))
+              (+ acc/2 1)
+              acc/2)))))))
 
 (let ((input (import-input)))
   (let ((part/1 (solve/1 input)))
