37850008a0abde7d618b4e6b01d5518b11fc0be3
turquoise-hexagon <camille@lzr.pw>
2024-12-05 11:37:25 +0000

    add 2024/05

--- 2024/05/solution.scm
+++ 2024/05/solution.scm
@@ -5,33 +5,36 @@
   (euler-syntax)
   (srfi 1))
 
+(define (comparator? rules page/a page/b)
+  (any
+    (lambda (rule)
+      (bind (rule/a rule/b) rule (and (= page/a rule/a) (= page/b rule/b))))
+    rules))
+
+(define (process-pages rules pages)
+  (map
+    (lambda (page)
+      (sort page (lambda (a b) (comparator? rules a b))))
+    pages))
+
+(define (process-input unsorted sorted)
+  (fold
+    (lambda (unsorted sorted acc)
+      (bind (invalid valid) acc
+        (if (equal? unsorted sorted)
+          (list invalid (cons sorted valid))
+          (list (cons sorted invalid) valid))))
+    '(() ()) unsorted sorted))
+
 (define (import-input)
   (bind (rules pages)
     (foldr
       (lambda (line acc)
-        (if (string=? line "")
-          (cons '() acc)
+        (if (string=? line "") (cons '() acc)
           (let ((item (map string->number (string-split line "|,"))))
             (cons (cons item (car acc)) (cdr acc)))))
       '(()) (read-lines))
-    (fold
-      (lambda (unsorted sorted acc)
-        (bind (invalid valid) acc
-          (if (equal? unsorted sorted)
-            (list invalid (cons sorted valid))
-            (list (cons sorted invalid) valid))))
-      '(() ()) pages
-      (map
-        (lambda (page)
-          (sort page
-            (lambda (page/a page/b)
-              (any
-                (lambda (rule)
-                  (bind (rule/a rule/b) rule
-                    (and (= page/a rule/a)
-                         (= page/b rule/b))))
-                rules))))
-        pages))))
+    (process-input pages (process-pages rules pages))))
 
 (define (solve input)
   (apply +
