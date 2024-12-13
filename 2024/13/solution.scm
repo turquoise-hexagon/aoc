131ec3421c540006eaeac1b1af041a502de3d338
turquoise-hexagon <camille@lzr.pw>
2024-12-13 23:08:44 +0000

    add 2024/13

--- 2024/13/solution.scm
+++ 2024/13/solution.scm
@@ -5,14 +5,14 @@
 
 (define (run lst offset)
   (bind (xa ya xb yb xp yp) lst
-   (let* ((xp (+ xp offset))
-          (yp (+ yp offset))
-          (a (/ (- (* xb yp) (* xp yb)) (- (* xb ya) (* xa yb))))
-          (b (/ (- (* xa yp) (* xp ya)) (- (* xa yb) (* xb ya)))))
-     (if (and (integer? a)
-              (integer? b))
-       (+ (* 3 a) b)
-       0))))
+    (let* ((xp (+ xp offset))
+           (yp (+ yp offset))
+           (a (/ (- (* xb yp) (* xp yb)) (- (* xb ya) (* xa yb))))
+           (b (/ (- (* xa yp) (* xp ya)) (- (* xa yb) (* xb ya)))))
+      (if (and (integer? a)
+               (integer? b))
+        (+ (* 3 a) b)
+        0))))
 
 (define (import-input)
   (chop (map string->number (irregex-extract "[0-9]+" (read-string))) 6))
