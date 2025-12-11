c7f3187f8df5304ae06c0f1ef595e9792936b4e7
turquoise-hexagon <camille@lzr.pw>
2025-12-11 15:35:16 +0000

    2025/11

--- 2025/11/solution.scm
+++ 2025/11/solution.scm
@@ -1,7 +1,8 @@
 (import
   (chicken io)
   (chicken string)
-  (srfi 69))
+  (srfi 69)
+  (euler-syntax))
 
 (define (import-input)
   (let ((acc (make-hash-table)))
@@ -20,27 +21,17 @@
       1
       (apply + (map loop (hash-table-ref/default input i '()))))))
 
-(define-inline (_loop i dac fft)
-  (if (and (equal? i 'out) dac fft)
-    1
-    (apply +
-      (map
-        (case i
-          ((dac) (lambda (i) (loop i  #t fft)))
-          ((fft) (lambda (i) (loop i dac  #t)))
-          (else  (lambda (i) (loop i dac fft))))
-        (hash-table-ref/default input i '())))))
-
 (define (solve/2 input)
-  (define loop
-    (let ((mem (make-hash-table)))
-      (lambda (i dac fft)
-        (let ((id (list i dac fft)))
-          (if (hash-table-exists? mem id)
-            (hash-table-ref mem id)
-            (let ((acc (_loop i dac fft)))
-              (hash-table-set! mem id acc)
-              acc))))))
+  (define-memoized (loop i dac fft)
+    (if (and (equal? i 'out) dac fft)
+      1
+      (apply +
+        (map
+          (case i
+            ((dac) (lambda (i) (loop i #t fft)))
+            ((fft) (lambda (i) (loop i dac #t)))
+            (else  (lambda (i) (loop i dac fft))))
+          (hash-table-ref/default input i '())))))
   (loop 'svr #f #f))
 
 (let* ((input (import-input))
